d1df9e4242706506eaf8fe5007f25cb8528d7e5f
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-11 23:29:16 +0100

    Fix tests

--- src/cli.js
+++ src/cli.js
@@ -2,17 +2,17 @@ const {red} = require('chalk')
 const cliCursor = require('cli-cursor')
 const endChildProcesses = require('end-child-processes')
 const minimist = require('minimist')
-const {tail} = require('prelude-ls')
 const TextRunner = require('./text-runner')
 
 cliCursor.hide()
 
 const argv = minimist(process.argv.slice(2))
-const commandsText = delete (argv._)
+const commandsText = argv._
+delete argv._
 const commands = (commandsText[0] || '').split(' ')
                                         .filter(it => it !== 'text-run')
 const textRunner = new TextRunner(argv)
-textRunner.execute((commands[0] || 'run'), tail(commands), (err) => {
+textRunner.execute((commands[0] || 'run'), commands.slice(1), (err) => {
   if (err && err.message !== 1) console.log(red(err))
 
   endChildProcesses()

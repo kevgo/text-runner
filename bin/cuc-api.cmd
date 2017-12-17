a060efbe6006710822164a1ab6eb26a0ce6a6ba7
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-17 17:42:48 +0100

    cleanup

--- bin/cuc-api.cmd
+++
@@ -1,2 +0,0 @@
-set EXOSERVICE_TEST_DEPTH=API
-node_modules\.bin\cucumber-js --tags ~@clionly --tags ~@todo --tags ~@skipWindows --format progress
--- bin/cuc-cli.cmd
+++
@@ -1,2 +0,0 @@
-set EXOSERVICE_TEST_DEPTH=CLI
-node_modules\.bin\cucumber-js --tags ~@apionly --tags ~@todo --tags ~@skipWindows --format pretty

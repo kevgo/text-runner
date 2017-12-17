df24076adb4a4b3a39ff92180f349f51fe55f9b4
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-17 20:36:11 +0100

    fix syntax

--- src/helpers/available-commands.js
+++ src/helpers/available-commands.js
@@ -4,6 +4,6 @@ const fs = require('fs')
 const path = require('path')
 
 // returns a list of all available commands
-module.exports = function() : string[] {
+module.exports = function () :string[] {
   return fs.readdirSync(path.join(__dirname, '..', 'commands'))
 }

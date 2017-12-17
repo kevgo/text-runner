0626cf1ac032d5ef8d7269d11e3e46237666f862
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-17 13:27:45 +0100

    Windows tests (#129)

--- bin/cuke-cli.cmd
+++ bin/cuke-cli.cmd
@@ -1,3 +1,3 @@
-node_modules\o-tools-livescript\bin\build
+call node_modules\o-tools-livescript\bin\build
 set EXOSERVICE_TEST_DEPTH=CLI
-node_modules\.bin\cucumber-js --tags ~@apionly --tags ~@todo --tags ~@skipWindows
+call node_modules\.bin\cucumber-js --tags ~@apionly --tags ~@todo --tags ~@skipWindows

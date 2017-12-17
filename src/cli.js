d216571b67330df474533cedc78199729e4f33c6
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-17 17:27:28 +0100

    update

--- src/cli.js
+++ src/cli.js
@@ -2,23 +2,16 @@ const {red} = require('chalk')
 const cliCursor = require('cli-cursor')
 const endChildProcesses = require('end-child-processes')
 const minimist = require('minimist')
+const parseCliArgs = require('./helpers/parse-cli-args')
+const pkg = require('../package.json')
 const TextRunner = require('./text-runner')
+const updateNotifier = require('update-notifier')
 
+updateNotifier({pkg}).notify()
 cliCursor.hide()
 
-const argv = minimist(process.argv.slice(2))
-const commandsText = argv._
-delete argv._
-const commands = (commandsText[0] || '').split(' ')
-                                        .filter(it => it !== 'text-run')
-const textRunner = new TextRunner(argv)
-textRunner.execute((commands[0] || 'run'), commands.slice(1), (err) => {
+textRunner(parseCliArgs(process.argv), (err) => {
   if (err && err.message !== 1) console.log(red(err))
-
   endChildProcesses()
-  if (err) {
-    process.exit(1)
-  } else {
-    process.exit(0)
-  }
+  process.exit(err ? 1 : 0)
 })

bd961fe1ae28af5b4a47ec2aceba5cad77cb2fc5
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-24 23:36:43 +0100

    rename

--- src/actions/built-in/cd.js
+++ src/actions/built-in/cd.js
@@ -5,28 +5,28 @@ const path = require('path')
 const debug = require('debug')('textrun:actions:cd')
 
 // Changes the current working directory to the one given in the hyperlink or code block
-module.exports = function (value: {configuration: Configuration, formatter: Formatter, searcher: Searcher}) {
-  value.formatter.start('changing the current working directory')
-  const directory = value.searcher.nodeContent({types: ['link_open', 'code']}, (v: {nodes: AstNodeList, content: string}): ?string => {
+module.exports = function (params: {configuration: Configuration, formatter: Formatter, searcher: Searcher}) {
+  params.formatter.start('changing the current working directory')
+  const directory = params.searcher.nodeContent({types: ['link_open', 'code']}, (v: {nodes: AstNodeList, content: string}): ?string => {
     if (v.nodes.length === 0) return 'no link found'
     if (v.nodes.length > 1) return 'too many links found'
     if (v.content.trim().length === 0) return 'empty link found'
     return null
   })
 
-  value.formatter.refine(`changing into the ${bold(cyan(directory))} directory`)
-  value.formatter.output(`cd ${directory}`)
+  params.formatter.refine(`changing into the ${bold(cyan(directory))} directory`)
+  params.formatter.output(`cd ${directory}`)
   try {
-    const fullPath = path.join(value.configuration.testDir, directory)
+    const fullPath = path.join(params.configuration.testDir, directory)
     debug(fullPath)
     process.chdir(fullPath)
-    value.formatter.success()
+    params.formatter.success()
   } catch (e) {
     debug(e)
     if (e.code === 'ENOENT') {
-      value.formatter.error(`directory ${directory} not found`)
+      params.formatter.error(`directory ${directory} not found`)
     } else {
-      value.formatter.error(e.message)
+      params.formatter.error(e.message)
     }
     throw e
   }

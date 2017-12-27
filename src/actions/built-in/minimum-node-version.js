484d2c0860551883c17cb29ea97bc27f9435b79c
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-27 17:45:47 +0100

    Make minimum-node-version action synchronous (#146)

--- src/actions/built-in/minimum-node-version.js
+++ src/actions/built-in/minimum-node-version.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const jsYaml = require('js-yaml')
 const {minimum} = require('prelude-ls')
 
-module.exports = function (params: {configuration: Configuration, formatter: Formatter, searcher: Searcher}, done: DoneFunction) {
+module.exports = function (params: {configuration: Configuration, formatter: Formatter, searcher: Searcher}) {
   params.formatter.start('determining minimum supported NodeJS version')
 
   const documentedVersion = parseInt(params.searcher.nodeContent({type: 'text'}, ({nodes, content}) => {
@@ -14,53 +14,32 @@ module.exports = function (params: {configuration: Configuration, formatter: For
   }))
   params.formatter.refine(`determining whether minimum supported NodeJS version is ${cyan(documentedVersion)}`)
 
-  getSupportedVersion((err, supportedVersion) => {
-    if (err) {
-      params.formatter.error(err)
-      done(err)
-      return
-    }
-    if (supportedVersion === documentedVersion) {
-      params.formatter.success(`requires at least Node ${cyan(supportedVersion)}`)
-      done()
-      return
-    }
-    if (supportedVersion !== documentedVersion) {
-      params.formatter.error(`documented minimum Node version is ${cyan(documentedVersion)}, should be ${cyan(supportedVersion)}`)
-      done(new Error('1'))
-    }
-  })
+  var supportedVersion
+  try {
+    supportedVersion = getSupportedVersion()
+  } catch (err) {
+    params.formatter.error(err)
+    throw new Error('1')
+  }
+  if (supportedVersion === documentedVersion) {
+    params.formatter.success(`requires at least Node ${cyan(supportedVersion)}`)
+    return
+  }
+  if (supportedVersion !== documentedVersion) {
+    params.formatter.error(`documented minimum Node version is ${cyan(documentedVersion)}, should be ${cyan(supportedVersion)}`)
+    throw new Error('1')
+  }
 }
 
-function getSupportedVersion (done) {
-  loadYmlFile('.travis.yml', (err, content) => {
-    if (err) {
-      done(err)
-      return
-    }
-    if (content == null) {
-      done(new Error('.travis.yml'))
-      return
-    }
-    const minimumVersion = parseInt(minimum(content.node_js))
-    if (minimumVersion === NaN) {
-      done(new Error('listed version is not a number'))
-      return
-    }
-    done(null, minimumVersion)
-  })
+function getSupportedVersion () {
+  const content = loadYmlFile('.travis.yml')
+  if (!content) throw new Error('.travis.yml is empty')
+  const minimumVersion = parseInt(minimum(content.node_js))
+  if (minimumVersion === NaN) throw new Error('listed version is not a number')
+  return minimumVersion
 }
 
-function loadYmlFile (filename: string, done) {
-  fs.readFile(filename, {encoding: 'utf8'}, (err: ?ErrnoError, fileContent: string) => {
-    if (err) {
-      done(err)
-      return
-    }
-    try {
-      done(null, jsYaml.safeLoad(fileContent))
-    } catch (e) {
-      done(e)
-    }
-  })
+function loadYmlFile (filename: string) {
+  const fileContent = fs.readFileSync(filename, {encoding: 'utf8'})
+  return jsYaml.safeLoad(fileContent)
 }

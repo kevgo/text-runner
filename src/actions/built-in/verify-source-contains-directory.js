b95b395665d0b45a37405ea49bbfddd7cfbc1fce
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-27 18:11:49 +0100

    Make verify-source-contains-directory synchronous (#150)

--- src/actions/built-in/verify-source-contains-directory.js
+++ src/actions/built-in/verify-source-contains-directory.js
@@ -5,23 +5,24 @@ const fs = require('fs')
 const path = require('path')
 
 // Verifies that a local directory linked in MarkDown exists
-module.exports = function (args: {formatter: Formatter, searcher: Searcher}, done: DoneFunction) {
+module.exports = function (args: {formatter: Formatter, searcher: Searcher}) {
   const directory = args.searcher.nodeContent({type: 'link_open'}, ({nodes}) => {
     if (nodes.length === 0) return 'no link found'
     if (nodes.length > 1) return 'too many links found'
   })
 
   args.formatter.start(`verifying the ${bold(cyan(directory))} directory exists in the source code`)
-  fs.lstat(path.join(process.cwd(), directory), (err, stats) => {
-    if (err) {
-      args.formatter.error(`directory ${cyan(bold(directory))} does not exist in the source code`)
-      done(new Error('1'))
-    } else if (stats.isDirectory()) {
-      args.formatter.success(`directory ${cyan(bold(directory))} exists in the source code`)
-      done()
-    } else {
-      args.formatter.error(`${cyan(bold(directory))} exists in the source code but is not a directory`)
-      done(new Error('1'))
-    }
-  })
+  var stats
+  try {
+    stats = fs.lstatSync(path.join(process.cwd(), directory))
+  } catch (err) {
+    args.formatter.error(`directory ${cyan(bold(directory))} does not exist in the source code`)
+    throw new Error('1')
+  }
+  if (stats.isDirectory()) {
+    args.formatter.success(`directory ${cyan(bold(directory))} exists in the source code`)
+  } else {
+    args.formatter.error(`${cyan(bold(directory))} exists in the source code but is not a directory`)
+    throw new Error('1')
+  }
 }

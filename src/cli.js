f3df67ff5e48a80cab4baa0dfba81394fccd64a4
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-29 11:45:34 -0800

    Re-enable dependency lint (#172)

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,8 @@ const {red} = require('chalk')
 const cliCursor = require('cli-cursor')
 const endChildProcesses = require('end-child-processes')
 const parseCliArgs = require('./helpers/parse-cli-args')
-// $FlowFixMe
-const pkg = require('../package.json')
 const textRunner = require('./text-runner')
-// $FlowFixMe
-const updateNotifier = require('update-notifier')
 
-updateNotifier({pkg}).notify()
 cliCursor.hide()
 
 textRunner(parseCliArgs(process.argv), (err) => {

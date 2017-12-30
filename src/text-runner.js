c1b70009f877b4c73963413c2369dfe81adc2c63
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-29 17:48:14 -0800

    fix

--- src/text-runner.js
+++ src/text-runner.js
@@ -5,6 +5,7 @@ const {red} = require('chalk')
 const commandPath = require('./helpers/command-path')
 const Configuration = require('./configuration')
 const FormatterManager = require('./formatters/formatter-manager')
+const fs = require('fs')
 const hasCommand = require('./helpers/has-command')
 const hasDirectory = require('./helpers/has-directory')
 const isGlob = require('is-glob')
@@ -13,7 +14,9 @@ const isMarkdownFile = require('./helpers/is-markdown-file')
 // Tests the documentation in the given directory
 module.exports = function (value: {command: string, file: string, fast: boolean, format: Formatter}, done: DoneFunction) {
   try {
-    const textRunner = new TextRunner({fast: value.fast, format: value.format}, 'text-run.yml')
+    var configFileName = ''
+    if (fs.existsSync('text-run.yml')) configFileName = 'text-run.yml'
+    const textRunner = new TextRunner({fast: value.fast, format: value.format}, configFileName)
     textRunner.execute(value.command, value.file, done)
   } catch (e) {
     done(e)

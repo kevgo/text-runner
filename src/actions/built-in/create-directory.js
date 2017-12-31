1c12e159901d9e746f8d98e4f21cf9f51d666c31
Kevin Goslar <kevin.goslar@gmail.com>
2017-12-31 03:17:34 -0800

    fix

--- src/actions/built-in/create-directory.js
+++ src/actions/built-in/create-directory.js
@@ -8,16 +8,16 @@ const debug = require('debug')('textrun:actions:create-directory')
 module.exports = function (params: {configuration: Configuration, formatter: Formatter, searcher: Searcher}) {
   params.formatter.start('creating directory')
 
-  const directoryName = params.searcher.nodeContent({type: 'code'}, ({nodes, content}) => {
-    if (nodes.length === 0) return 'no name given for directory to create'
-    if (nodes.length > 1) return `several names given: ${nodes.map((node) => node.content).map((a) => cyan(a)).join(' and ')}`
-    if (!content) return 'empty name given for directory to create'
-  })
-
-  params.formatter.refine(`creating directory ${cyan(directoryName)}`)
-  const fullPath = path.join(params.configuration.testDir, directoryName)
-  debug(fullPath)
   try {
+    const directoryName = params.searcher.nodeContent({type: 'code'}, ({nodes, content}) => {
+      if (nodes.length === 0) return 'no name given for directory to create'
+      if (nodes.length > 1) return `several names given: ${nodes.map((node) => node.content).map((a) => cyan(a)).join(' and ')}`
+      if (!content) return 'empty name given for directory to create'
+    })
+
+    params.formatter.refine(`creating directory ${cyan(directoryName)}`)
+    const fullPath = path.join(params.configuration.testDir, directoryName)
+    debug(`directory to create: ${fullPath}`)
     mkdirp.sync(fullPath)
     params.formatter.success()
   } catch (err) {

11e022f11dc9ee9e4d82282cfe28824635cbc494
Kevin Goslar <kevin.goslar@gmail.com>
2018-01-04 13:56:52 -0800

    Cleanup activity list builder (#199)

--- src/commands/run/activity-list-builder.js
+++ src/commands/run/activity-list-builder.js
@@ -23,28 +23,30 @@ class ActivityListBuilder {
   }
 
   build (tree: AstNodeList): ActivityList {
-    var nodesForCurrentRunner: ?AstNodeList
+    var insideActiveBlock = false                    // whether we are currently processing nodes of an active block
+    var nodesForCurrentRunner: AstNodeList = []
     var startLine = 0
     var result: ActivityList = []
-    var currentRunnerType: ?Action
+    var currentRunnerType: Action = (value) => {}
     for (let node: AstNode of tree) {
-      // link start tag
-      const blockType : ?string = this._isStartTag(node)
+      // active block start tag
+      const blockType : ?string = this._isActiveBlockStartTag(node)
       if (blockType != null) {
         startLine = node.line
-        if (currentRunnerType) {
+        if (insideActiveBlock) {
           this.formatter.error('Found a nested <a class="tr_*"> block')
           return []
         }
         currentRunnerType = this.actions.actionFor(blockType, this.filePath)
         if (currentRunnerType) {
+          insideActiveBlock = true
           nodesForCurrentRunner = []
         }
         continue
       }
 
-      if (this._isEndTag(node)) {
-        if (currentRunnerType) {
+      if (this._isActiveBlockEndTag(node)) {
+        if (insideActiveBlock) {
           result.push({
             filename: this.filePath,
             startLine: startLine,
@@ -53,28 +55,21 @@ class ActivityListBuilder {
             nodes: nodesForCurrentRunner,
             formatter: this.formatter,
             configuration: this.configuration,
-            searcher: new Searcher({
-              filePath: this.filePath,
-              startLine: startLine,
-              endLine: node.line,
-              nodes: nodesForCurrentRunner,
-              formatter: this.formatter
-            })
+            searcher: new Searcher(nodesForCurrentRunner)
           })
         }
-        currentRunnerType = null
-        nodesForCurrentRunner = null
+        insideActiveBlock = false
+        nodesForCurrentRunner = []
         continue
       }
 
-      if (currentRunnerType) {
-        if (nodesForCurrentRunner) {
-          nodesForCurrentRunner.push(node)
-        }
+      if (insideActiveBlock) {
+        nodesForCurrentRunner.push(node)
         continue
       }
 
       if (node.type === 'image') {
+        // push 'check image' activity
         result.push({
           filename: this.filePath,
           startLine: node.line,
@@ -82,12 +77,14 @@ class ActivityListBuilder {
           nodes: [node],
           runner: this.actions.actionFor('checkImage', this.filePath),
           formatter: this.formatter,
-          configuration: this.configuration
+          configuration: this.configuration,
+          searcher: new Searcher([node])
         })
         continue
       }
 
       if (this._isMarkdownLink(node)) {
+        // push 'check link' activity for Markdown links
         result.push({
           filename: this.filePath,
           startLine: node.line,
@@ -96,13 +93,15 @@ class ActivityListBuilder {
           runner: this.actions.actionFor('checkLink', this.filePath),
           formatter: this.formatter,
           configuration: this.configuration,
-          linkTargets: this.linkTargets
+          linkTargets: this.linkTargets,
+          searcher: new Searcher([node])
         })
         continue
       }
 
       const target = this._htmlLinkTarget(node)
       if (target != null) {
+        // push 'check link' activity for HTML links
         result.push({
           filename: this.filePath,
           startLine: node.line,
@@ -111,7 +110,8 @@ class ActivityListBuilder {
           runner: this.actions.actionFor('checkLink', this.filePath),
           formatter: this.formatter,
           configuration: this.configuration,
-          linkTargets: this.linkTargets
+          linkTargets: this.linkTargets,
+          searcher: new Searcher([node])
         })
         continue
       }
@@ -135,7 +135,7 @@ class ActivityListBuilder {
 
   // Indicates whether the given node is a start tag of an active block
   // by returning the type of the block, or falsy.
-  _isStartTag (node): ?string {
+  _isActiveBlockStartTag (node): ?string {
     if (node.type !== 'htmltag') return null
     const regex = new RegExp(`<a class="${this.configuration.get('classPrefix')}([^"]+)">`)
     if (node.content == null) return null
@@ -145,7 +145,7 @@ class ActivityListBuilder {
   }
 
   // Returns whether the given node is the end of an active block
-  _isEndTag (node) {
+  _isActiveBlockEndTag (node) {
     return node.type === 'htmltag' && node.content === '</a>'
   }
 }
--- src/commands/run/searcher.js
+++ src/commands/run/searcher.js
@@ -1,6 +1,5 @@
 // @flow
 
-const Formatter = require('../../formatters/formatter.js')
 const UnprintedUserError = require('../../errors/unprinted-user-error.js')
 
 type ErrorCheckerFunc = (value: {nodes: AstNodeList, content: string}) => ?string
@@ -10,14 +9,9 @@ class Searcher {
   startLine: ?number
   endLine: ?number
   nodes: ?AstNodeList
-  formatter: Formatter
 
-  constructor (value: {filePath: string, startLine: ?number, endLine: ?number, nodes: ?AstNodeList, formatter: Formatter}) {
-    this.filePath = value.filePath
-    this.startLine = value.startLine
-    this.endLine = value.endLine
-    this.nodes = value.nodes
-    this.formatter = value.formatter
+  constructor (nodes: AstNodeList) {
+    this.nodes = nodes
   }
 
   nodeContent (query: {type: ?string, types: ?string[]}, errorChecker: ErrorCheckerFunc): string {

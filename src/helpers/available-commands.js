364e5045a74330c5bf0c97098d44d6d042858a94
Kevin Goslar <kevin.goslar@gmail.com>
2018-01-05 06:48:34 -0800

    stuff

--- src/helpers/available-commands.js
+++ src/helpers/available-commands.js
@@ -5,5 +5,6 @@ const path = require('path')
 
 // returns a list of all available commands
 module.exports = function () :string[] {
-  return fs.readdirSync(path.join(__dirname, '..', 'commands'))
+  const commandDir = path.join(__dirname, '..', 'commands')
+  return fs.readdirSync(commandDir).filter(f => fs.statSync(path.join(commandDir, f)).isDirectory())
 }

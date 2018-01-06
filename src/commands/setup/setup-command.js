391a8db6da9e5d351f49a07faa9f2d720fe2cd95
Kevin Goslar <kevin.goslar@gmail.com>
2018-01-06 15:29:12 -0800

    Cleanup types

--- src/commands/setup/setup-command.js
+++ src/commands/setup/setup-command.js
@@ -18,7 +18,7 @@ class SetupCommand implements Command {
     this.actions = value.actions
   }
 
-  async run (): Promise<?ErrnoError> {
+  async run () {
     this.formatter.startActivity(`Create configuration file ${cyan('text-run.yml')} with default values`)
     this.configuration.createDefault()
     this.formatter.success()

a3d07dc4f45fe9d8cfe7335d2c85a6433593094e
Kevin Goslar <kevin.goslar@gmail.com>
2018-01-07 06:35:00 -0800

    make it work

---
+++ text-run/cdBack.js
@@ -0,0 +1,5 @@
+module.exports = async function ({formatter}) {
+  if (global.cdHistory == null) throw new Error('no CD history')
+  formatter.output('cd ' + global.cdHistory)
+  process.chdir(global.cdHistory)
+}

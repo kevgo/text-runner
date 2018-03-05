23f85241fc4a9f3942ec7dffd97ecbc0c928985d
Kevin Goslar <kevin.goslar@gmail.com>
2018-03-05 09:15:18 -0800

    format

--- text-run/create-markdown-file.js
+++ text-run/create-markdown-file.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function ({formatter, configuration, searcher}) {
+module.exports = function ({ formatter, configuration, searcher }) {
   const markdown = searcher.tagContent('fence').replace(/​/g, '')
   fs.writeFileSync(path.join(configuration.testDir, '1.md'), markdown)
 }

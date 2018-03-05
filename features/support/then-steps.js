6513f911ba0f03a80579022fc623be0ff1d8a638
Kevin Goslar <kevin.goslar@gmail.com>
2018-03-05 12:41:48 -0500

    Cucumber expressions (#315)

--- features/support/then-steps.js
+++ features/support/then-steps.js
@@ -1,101 +1,117 @@
 // @flow
 
-const {Then} = require('cucumber')
-const {expect} = require('chai')
+const { Then } = require('cucumber')
+const { expect } = require('chai')
 const fs = require('fs-extra')
 const jsdiffConsole = require('jsdiff-console')
 const path = require('path')
 
-Then(/^I see usage instructions$/, function () {
+Then('I see usage instructions', function () {
   this.verifyPrintedUsageInstructions()
 })
 
-Then(/^it creates a directory "([^"]*)"$/, function (directoryPath) {
+Then('it creates a directory {string}', function (directoryPath) {
   fs.statSync(path.join(this.rootDir, directoryPath))
 })
 
-Then(/^it creates the file "([^"]*)" with content:$/, function (filename, expectedContent) {
-  const actualContent = fs.readFileSync(path.join(this.rootDir, filename),
-                                          {encoding: 'utf8'})
+Then('it creates the file {string} with content:', function (
+  filename,
+  expectedContent
+) {
+  const actualContent = fs.readFileSync(path.join(this.rootDir, filename), {
+    encoding: 'utf8'
+  })
   jsdiffConsole(actualContent.trim(), expectedContent.trim())
 })
 
-Then(/^it doesn't print:$/, function (expectedText) {
+Then("it doesn't print:", function (expectedText) {
   this.verifyPrintsNot(expectedText)
 })
 
-Then('it generates the file {string} with content:', function (filename, expectedContent) {
-  const actualContent = fs.readFileSync(path.join(this.rootDir, filename), 'utf8')
+Then('it generates the file {string} with content:', function (
+  filename,
+  expectedContent
+) {
+  const actualContent = fs.readFileSync(
+    path.join(this.rootDir, filename),
+    'utf8'
+  )
   jsdiffConsole(actualContent, expectedContent)
 })
 
-Then(/^it prints:$/, function (expectedText) {
+Then('it prints:', function (expectedText) {
   this.verifyPrints(expectedText)
 })
 
-Then(/^it prints the error message:$/, function (expectedText) {
+Then('it prints the error message:', function (expectedText) {
   this.verifyErrormessage(expectedText)
 })
 
-Then(/^it runs (\d+) test$/, function (count) {
+Then('it runs {int} test', function (count) {
   this.verifyTestsRun(count)
 })
 
-Then(/^it runs in a global temp directory$/, function () {
+Then('it runs in a global temp directory', function () {
   expect(this.output).to.not.include(this.rootDir)
 })
 
-Then(/^it runs in the "([^"]+)" directory$/, function (dirName) {
-  expect(this.output).to.match(new RegExp(`${dirName}\\b`))
+Then('it runs in the {string} directory', function (dirName) {
+  expect(this.output).to.match(new RegExp(`\\b${dirName}\\b`))
 })
 
-Then(/^it runs in the current working directory$/, function () {
-  expect(this.output).to.match(new RegExp(`${this.rootDir}\\b`))
+Then('it runs in the current working directory', function () {
+  expect(this.output.trim()).to.match(new RegExp(`${this.rootDir}\\b`))
 })
 
 Then(/^it runs(?: only)? the tests in "([^"]*)"$/, function (filename) {
   this.verifyRanOnlyTests([filename])
 })
 
-Then(/^it runs only the tests in:$/, function (table) {
+Then('it runs only the tests in:', function (table) {
   this.verifyRanOnlyTests(table.raw())
 })
 
-Then(/^it runs the console command "([^"]*)"$/, async function (command) {
+Then('it runs the console command {string}', async function (command) {
   this.verifyRanConsoleCommand(command)
 })
 
-Then(/^it runs without errors$/, function () {
-})
+Then('it runs without errors', function () {})
 
-Then(/^it signals:$/, function (table) {
+Then('it signals:', function (table) {
   this.verifyOutput(table.rowsHash())
 })
 
-Then(/^the call fails with the error:$/, function (expectedError) {
+Then('the call fails with the error:', function (expectedError) {
   this.verifyCallError(expectedError)
 })
 
-Then(/^the current working directory is now "([^"]*)"$/, function (expectedCwd) {
+Then('the current working directory is now {string}', function (expectedCwd) {
   expect(path.basename(this.cwdAfterRun)).to.equal(expectedCwd)
 })
 
-Then('the {string} directory is now deleted', function(directoryPath) {
+Then('the {string} directory is now deleted', function (directoryPath) {
   try {
     fs.statSync(path.join(this.rootDir, directoryPath))
     throw new Error(`file '${directoryPath}' still exists`)
   } catch (e) {}
 })
 
-Then(/^the test directory (?:now |still )contains a file "([^"]*)" with content:$/, function (fileName, expectedContent) {
-  expect(fs.readFileSync(path.join(this.rootDir, 'tmp', fileName), 'utf8').trim()).to.equal(expectedContent.trim())
-})
+Then(
+  /^the test directory (?:now |still )contains a file "([^"]*)" with content:$/,
+  function (fileName, expectedContent) {
+    expect(
+      fs.readFileSync(path.join(this.rootDir, 'tmp', fileName), 'utf8').trim()
+    ).to.equal(expectedContent.trim())
+  }
+)
 
-Then(/^the test workspace now contains a directory "([^"]*)"$/, function (fileName) {
+Then('the test workspace now contains a directory {string}', function (
+  fileName
+) {
   const stat = fs.statSync(path.join(this.rootDir, 'tmp', fileName))
   expect(stat.isDirectory()).to.be.true
 })
 
-Then(/^the test fails with:$/, function (table) {
+Then('the test fails with:', function (table) {
   this.verifyFailure(table.rowsHash())
 })

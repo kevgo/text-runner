c43f82376c74190a1795514aa6458067c5dffea2
Kevin Goslar <kevin.goslar@gmail.com>
2018-03-19 10:43:46 -0700

    temp

--- bin/cleanse-coverage.js
+++ bin/cleanse-coverage.js
@@ -4,15 +4,18 @@
 const fs = require('fs-extra')
 const jsonfile = require('jsonfile')
 const path = require('path')
+const { promisify } = require('util')
+const readjson = promisify(jsonfile.readFile)
+const writejson = promisify(jsonfile.writeFile)
 
-async function main() {
+async function main () {
   const directories = ['.nyc_output_api']
   // const directories = ['.nyc_output_tests', '.nyc_output_api', '.nyc_output_text_run']
   // directories.concat(fs.readdirSync('.nyc_output_cli'))
   await Promise.all(directories.map(mergeAndCleanseDir))
 }
 
-async function mergeAndCleanseDir(dir) {
+async function mergeAndCleanseDir (dir) {
   console.log(dir)
   let files = await fs.readdir(dir)
   files = files.map(file => path.join(dir, file))
@@ -20,18 +23,18 @@ async function mergeAndCleanseDir(dir) {
   files.forEach(mergeAndCleanseFile)
 }
 
-async function mergeAndCleanseFile(filename) {
+async function mergeAndCleanseFile (filename) {
   const filepath = path.join(process.cwd(), filename)
-  console.log('filepath:', filepath)
-  const filedata = await jsonfile.readFile(filepath)
-  console.log(filedata)
+  const filedata = await readjson(filepath)
   const result = {}
   for (let key of Object.keys(filedata)) {
     if (re.test(key)) continue
     result[key] = filedata[key]
   }
 
-  await jsonfile.writeFile(path.join('.nyc_output', filename), JSON.stringify(result))
+  const writepath = path.join('.nyc_output', filename)
+  console.log(writepath)
+  await writejson(writepath, JSON.stringify(result))
 }
 
 const re = /\/dist\//

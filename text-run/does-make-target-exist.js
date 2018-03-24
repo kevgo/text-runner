c7bcfb37ab7ab0ebef60203428ee735bee4f0fa8
Kevin Goslar <kevin.goslar@gmail.com>
2018-03-24 20:18:12 +0100

    cleanup

--- text-run/does-make-target-exist.js
+++ text-run/does-make-target-exist.js
@@ -3,12 +3,13 @@ const exec = util.promisify(require('child_process').exec)
 
 module.exports = async function ({ formatter, searcher }) {
   const makeExpression = searcher.tagContent('code')
-  const target = makeExpression.split(' ')[1]
+  const expected = makeExpression.split(' ')[1]
   const { stdout, stderr } = await exec('make help')
   if (stderr.length > 0) {
     throw new Error(`Error running 'make help': ${stderr}`)
   }
-  if (!stdout.includes(target)) {
-    throw new Error(`binary '${target}' does not exist`)
+  const actuals = stdout.split('\n')
+  if (!actuals.includes(expected)) {
+    throw new Error(`binary '${expected}' does not exist`)
   }
 }

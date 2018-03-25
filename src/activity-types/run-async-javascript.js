a59a48b18e652ec92fdaec3063eff59e92e859b8
Kevin Goslar <kevin.goslar@gmail.com>
2018-03-25 15:41:07 +0200

    Async Javascript

---
+++ src/activity-types/run-async-javascript.js
@@ -0,0 +1,55 @@
+// @flow
+
+import type { Activity } from '../commands/run/activity.js'
+import type Configuration from '../configuration/configuration.js'
+
+type DoneFunction = (err?: ?ErrnoError) => void
+
+// Runs the async-await JavaScript code given in the code block
+module.exports = function (activity: Activity, done: DoneFunction) {
+  var code = activity.searcher.tagContent('fence')
+  if (code == null) {
+    done(new Error('no JavaScript code found in the fenced block'))
+    return
+  }
+  code = replaceSubstitutions(code, activity.configuration)
+  code = replaceRequireLocalModule(code)
+  code = replaceVariableDeclarations(code)
+  code = wrapInAsyncFunction(code)
+
+  activity.formatter.output(code)
+  /* eslint-disable no-eval */
+  eval(code)
+}
+
+function wrapInAsyncFunction (code) {
+  return `async function() {
+    ${code}
+  }()`
+}
+
+// substitutes replacements configured in text-run.yml
+function replaceSubstitutions (code: string, c: Configuration): string {
+  try {
+    // $FlowFixMe: we can ignore undefined values here since `code` has a default value
+    for (let replaceData of c.fileData.actions.runJavascript.replace) {
+      code = code.replace(replaceData.search, replaceData.replace)
+    }
+  } catch (e) {
+    // ignoring type errors here since `code` has a default value
+  }
+  return code
+}
+
+// makes sure "require('.') works as expected even if running in a temp workspace
+function replaceRequireLocalModule (code: string): string {
+  return code.replace(/require\(['"].['"]\)/, 'require(process.cwd())')
+}
+
+// make variable declarations persist across code blocks
+function replaceVariableDeclarations (code: string): string {
+  return code
+    .replace(/\bconst /g, 'global.')
+    .replace(/\bvar /g, 'global.')
+    .replace(/\bthis\./g, 'global.')
+}

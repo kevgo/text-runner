bf7e5bca0f7a8583b4c7e2c34c85421394da32f2
Kevin Goslar <kevin.goslar@gmail.com>
2018-04-23 13:55:39 -0700

    types

--- src/commands/run/2-read-and-parse/read-and-parse-file.js
+++ src/commands/run/2-read-and-parse/read-and-parse-file.js
@@ -1,9 +1,11 @@
 // @flow
 
-const parseMarkdown = require('./parse-markdown.js')
+import type { AstNodeList } from './ast-node-list.js'
+
+const parseMarkdown = require('./parsers/markdown/parse-markdown.js')
 const readFile = require('../../../helpers/read-file.js')
 
-module.exports = async function (filename: string) {
+module.exports = async function (filename: string): Promise<AstNodeList> {
   const content = await readFile(filename)
   return parseMarkdown(content, filename)
 }

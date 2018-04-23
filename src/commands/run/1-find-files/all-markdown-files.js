859b6fcf7f6509f552cb0d54117cc966d1a0ef50
Kevin Goslar <kevin.goslar@gmail.com>
2018-04-23 13:34:14 -0700

    move

--- src/commands/run/1-find-files/all-markdown-files.js
+++ src/commands/run/1-find-files/all-markdown-files.js
@@ -6,8 +6,5 @@ const glob = require('glob')
 module.exports = function allMarkdownFiles (configuredFiles: string): string[] {
   var files = glob.sync(configuredFiles)
   files = files.filter(file => !file.includes('node_modules')).sort()
-  // if (this.filename != null) {
-  //   files = files.filter(file => !file === this.filename)
-  // }
   return files
 }
--- src/commands/run/2-read-and-parse/read-and-parse-file.js
+++ src/commands/run/2-read-and-parse/read-and-parse-file.js
@@ -1,7 +1,7 @@
 // @flow
 
 const parseMarkdown = require('./parse-markdown.js')
-const readFile = require('./read-file.js')
+const readFile = require('../../../helpers/read-file.js')
 
 module.exports = async function (filename: string) {
   const content = await readFile(filename)

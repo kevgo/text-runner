10b65711ce65791daf1b1289e39fb0ca928679db
Kevin Goslar <kevin.goslar@gmail.com>
2018-04-29 16:46:50 -0700

    cleanup

--- src/parsers/helpers/end-type-for.js
+++
@@ -1,6 +0,0 @@
-// @flow
-
-// Returns the node type closing the given node type
-module.exports = function endTypeFor (nodeType: string): string {
-  return nodeType.replace('open', '') + 'close'
-}

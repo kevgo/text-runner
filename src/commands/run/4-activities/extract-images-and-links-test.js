82c1f75b23372342db8d67c70c8a9ffeef572e39
Kevin Goslar <kevin.goslar@gmail.com>
2018-05-07 19:17:51 -0700

    fix tests

--- src/commands/run/4-activities/extract-images-and-links-test.js
+++ src/commands/run/4-activities/extract-images-and-links-test.js
@@ -1,13 +1,13 @@
 // $flow
 
 const AstNodeList = require('../../../parsers/ast-node-list.js')
-const extract = require('./extract-images-and-links.js')
+const extractImagesAndLinks = require('./extract-images-and-links.js')
 
 describe('extractImagesAndLinks', function () {
   it('extracts images', function () {
     const nodes = new AstNodeList()
     nodes.scaffold({ type: 'image', file: 'filename', line: 3 })
-    const result = extract(nodes)
+    const result = extractImagesAndLinks([nodes])
     expect(result).to.have.length(1)
     expect(result[0].type).to.equal('check-image')
     expect(result[0].file).to.equal('filename')
@@ -21,7 +21,7 @@ describe('extractImagesAndLinks', function () {
     nodes.scaffold({ type: 'link_open', file: 'filename', line: 3 })
     nodes.scaffold({ type: 'text', file: 'filename', line: 3, content: 'foo' })
     nodes.scaffold({ type: 'link_close', file: 'filename', line: 3 })
-    const result = extract(nodes)
+    const result = extractImagesAndLinks([nodes])
     expect(result).to.have.length(1)
     expect(result[0].type).to.equal('check-link')
     expect(result[0].file).to.equal('filename')

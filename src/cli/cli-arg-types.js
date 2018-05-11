8f2ae65c082e80519789711913e0a79e49a2d1f2
Kevin Goslar <kevin.goslar@gmail.com>
2018-05-11 06:20:31 -0700

    make features work

--- src/cli/cli-arg-types.js
+++ src/cli/cli-arg-types.js
@@ -5,7 +5,7 @@ import Formatter from '../formatters/formatter.js'
 export type CliArgTypes = {
   command: string,
   exclude?: string,
-  file?: string,
+  files?: string,
   format?: Formatter,
   offline?: boolean
 }
---
+++ src/parsers/markdown/transformers/md/link_close.js
@@ -0,0 +1,24 @@
+// @flow
+
+const AstNodeList = require('../../../ast-node-list.js')
+const OpenTagTracker = require('../../helpers/open-tag-tracker.js')
+
+module.exports = function (
+  node: Object,
+  openTags: OpenTagTracker,
+  file: string,
+  line: number
+): AstNodeList {
+  // TODO: remove openTags, since MD nodes never have attributes
+  const openNode = openTags.pop('link_open')
+  const result = new AstNodeList()
+  result.pushData({
+    type: node.type,
+    tag: '/a',
+    file,
+    line,
+    content: '',
+    attributes: openNode.attributes
+  })
+  return result
+}

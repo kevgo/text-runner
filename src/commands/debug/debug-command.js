0c85a8422718bd02dacec8e98ffa351f6ee0bfeb
Kevin Goslar <kevin.goslar@gmail.com>
2018-05-21 23:23:22 -0500

    Debug command

--- src/commands/debug/debug-command.js
+++ src/commands/debug/debug-command.js
@@ -2,7 +2,8 @@
 
 import type { Configuration } from '../../configuration/configuration.js'
 
-const { magenta } = require('chalk')
+const AstNode = require('../../parsers/ast-node.js')
+
 const extractActivities = require('../../activity-list/extract-activities.js')
 const extractImagesAndLinks = require('../../activity-list/extract-images-and-links.js')
 const getFileNames = require('../../finding-files/get-filenames.js')
@@ -12,13 +13,40 @@ async function debugCommand (config: Configuration) {
   const filenames = getFileNames(config)
   if (filenames.length === 0) return
 
+  console.log('AST NODES:')
   const ASTs = await Promise.all(filenames.map(readAndParseFile))
-
-  const activities = extractActivities(ASTs, config.classPrefix)
-  const links = extractImagesAndLinks(ASTs)
-  if (activities.length === 0 && links.length === 0) {
-    console.log(magenta('no activities found'))
+  for (const AST of ASTs) {
+    for (const node of AST) {
+      console.log(`${node.file}:${node.line}  ${node.type} ${showAttr(node)}`)
+    }
   }
+
+  console.log('\nIMAGES AND LINKS:')
+  const links = extractImagesAndLinks(ASTs)
+  if (links.length === 0) {
+    console.log('(none)')
+  } else {
+    for (const link of links) {
+      console.log(link)
+    }
+  }
+
+  console.log('\nACTIVITIES:')
+  const activities = extractActivities(ASTs, config.classPrefix)
+  if (activities.length === 0) {
+    console.log('(none)')
+  } else {
+    for (const activity of activities) {
+      console.log(`${activity.file}:${activity.line}  ${activity.type}`)
+    }
+  }
+}
+
+function showAttr (node: AstNode): string {
+  if (node.type === 'text') return `("${node.content.trim()}")`
+  const keys = Object.keys(node.attributes)
+  if (keys.length === 0) return ''
+  return `(${node.attributes['textrun']})`
 }
 
 module.exports = debugCommand

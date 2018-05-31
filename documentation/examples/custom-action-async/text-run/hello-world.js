d56c12c87be17bbe7291483bc328fde43c180b8d
Kevin Goslar <kevin.goslar@gmail.com>
2018-05-31 13:33:17 -0700

    Make custom action examples conform to type

--- documentation/examples/custom-action-async/text-run/hello-world.js
+++ documentation/examples/custom-action-async/text-run/hello-world.js
@@ -2,6 +2,6 @@ const delay = require('delay')
 
 module.exports = async ({ formatter }) => {
   await delay(1)
-  formatter.output('Hello World!')
+  formatter.log('Hello World!')
   await delay(1)
 }

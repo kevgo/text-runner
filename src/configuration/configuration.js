01389926f2a744bad11f51dc3731aff16e1001f9
Kevin Goslar <kevin.goslar@gmail.com>
2018-05-31 08:13:35 -0700

    sort alphabetically

--- src/configuration/configuration.js
+++ src/configuration/configuration.js
@@ -4,13 +4,13 @@ const Formatter = require('../formatters/formatter.js')
 
 // Data structure for configuration values
 export type Configuration = {|
-  offline: boolean, // whether to skip built-in tests that require a network connection
-  fileGlob: string, // glob of the files to test
+  activityTypes: Object, // activity-specific configuration
+  classPrefix: string, // the name of the attribute that denotes active blocks
   exclude: string | string[], // list of names or regexes of files to exclude
+  fileGlob: string, // glob of the files to test
   FormatterClass: typeof Formatter, // type of the Formatter class to use
+  offline: boolean, // whether to skip built-in tests that require a network connection
   sourceDir: string, // the root directory of the source code to test
   useSystemTempDirectory: boolean, // whether to create the workspace in the system temp directory or locally
-  workspace: string, // the root directory of the workspace
-  classPrefix: string, // the name of the attribute that denotes active blocks
-  activityTypes: Object // activity-specific configuration
+  workspace: string // the root directory of the workspace
 |}

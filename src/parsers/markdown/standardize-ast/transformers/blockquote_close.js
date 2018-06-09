ebb28a1c8bc5200e43b42dcd3d7d07d7bdd83573
Kevin Goslar <kevin.goslar@gmail.com>
2018-06-09 09:53:50 -0400

    Blockquote HTML tag

---
+++ src/parsers/markdown/standardize-ast/transformers/blockquote_close.js
@@ -0,0 +1,23 @@
+// @flow
+
+const AstNodeList = require('../../../ast-node-list.js')
+const OpenTagTracker = require('../../helpers/open-tag-tracker.js')
+
+module.exports = function (
+  node: Object,
+  openTags: OpenTagTracker,
+  file: string,
+  line: number
+): AstNodeList {
+  const result = new AstNodeList()
+  const openNode = openTags.pop('bullet_list_open')
+  result.pushData({
+    type: node.type,
+    tag: '/blockquote',
+    file,
+    line,
+    content: '',
+    attributes: openNode.attributes
+  })
+  return result
+}

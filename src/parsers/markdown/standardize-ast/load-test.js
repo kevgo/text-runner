630eb98549e5e6889ae3aa3e68370e46e0a63b52
Kevin Goslar <kevin.goslar@gmail.com>
2018-06-09 09:02:57 -0400

    Rename transformers

--- src/parsers/markdown/standardize-ast/load-test.js
+++ src/parsers/markdown/standardize-ast/load-test.js
@@ -5,7 +5,7 @@ const { expect } = require('chai')
 
 describe('loadTransformers', function () {
   it('loads MD files', async function () {
-    const result = await loadTransformers('md')
+    const result = await loadTransformers()
     expect(Object.keys(result).length).to.be.above(0)
   })
 })

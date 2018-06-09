b58f2ba3a4012a2fc1b531c0a4c7df8f12781a0b
Kevin Goslar <kevin.goslar@gmail.com>
2018-06-09 09:17:45 -0400

    Ordered list AST nodes

---
+++ src/parsers/markdown/standardize-ast/transformers/ordered_list_close.js
@@ -0,0 +1,23 @@
+// @flow
+
+const AstNodeList = require('../../../ast-node-list.js')
+const OpenTagTracker = require('../../helpers/open-tag-tracker.js')
+
+module.exports = function (
+  node: Object,
+  openTags: OpenTagTracker,
+  file: string,
+  line: number
+): AstNodeList {
+  const result = new AstNodeList()
+  const openNode = openTags.pop('ordered_list_open')
+  result.pushData({
+    type: node.type,
+    tag: '/ol',
+    file,
+    line,
+    content: '',
+    attributes: openNode.attributes
+  })
+  return result
+}

6cb423bfca5834cac9e9f006d02476603654c747
Kevin Goslar <kevin.goslar@gmail.com>
2018-06-10 10:03:47 -0400

    Support active HR tags

---
+++ src/parsers/markdown/standardize-ast/transformers-htmlblock/hr.js
@@ -0,0 +1,28 @@
+// @flow
+
+const AstNode = require('../../../ast-node.js')
+const AstNodeList = require('../../../ast-node-list.js')
+const parseHtmlAttributes = require('../../helpers/parse-html-attributes.js')
+const OpenTagTracker = require('../../helpers/open-tag-tracker.js')
+
+const olRegex = /<hr([^>]*)>/
+
+module.exports = async function transformOl (
+  node: Object,
+  openTags: OpenTagTracker,
+  file: string,
+  line: number
+): Promise<AstNodeList> {
+  const result = new AstNodeList()
+  const match = node.content.match(olRegex)
+  const hrNode = new AstNode({
+    type: 'horizontal_row',
+    tag: 'hr',
+    file,
+    line,
+    content: '',
+    attributes: parseHtmlAttributes(match[1])
+  })
+  result.pushData(hrNode)
+  return result
+}

e12ef51ab84ad339f38d90de71ced8c5fa02f7f8
Kevin Goslar <kevin.goslar@gmail.com>
2018-06-10 18:33:15 -0400

    Sort config options alphabetically

--- src/configuration/configuration.js
+++ src/configuration/configuration.js
@@ -8,8 +8,8 @@ export type Configuration = {|
   classPrefix: string, // the name of the attribute that denotes active blocks
   exclude: string | string[], // list of names or regexes of files to exclude
   fileGlob: string, // glob of the files to test
-  keepTmp: boolean, // whether to keep the tmp dir if tests successful
   FormatterClass: typeof Formatter, // type of the Formatter class to use
+  keepTmp: boolean, // whether to keep the tmp dir if tests successful
   offline: boolean, // whether to skip built-in tests that require a network connection
   sourceDir: string, // the root directory of the source code to test
   useSystemTempDirectory: boolean, // whether to create the workspace in the system temp directory or locally

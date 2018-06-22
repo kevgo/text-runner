c1ef793e1895384bf575e12de3b936f96686f410
Kevin Goslar <kevin.goslar@originate.com>
2018-06-22 10:55:46 -0700

    stuff?

--- features/support/world.js
+++ features/support/world.js
@@ -38,9 +38,10 @@ const World = function () {
       args.env['DEBUG'] = '*,-babel'
     }
 
-    args.command = this.makeFullPath(params.command)
     if (process.env.NODE_ENV === 'coverage') {
       args.command = runWithTestCoverage(args.command)
+    } else {
+      args.command = this.makeFullPath(params.command)
     }
     this.process = new ObservableProcess(args)
     await this.process.waitForEnd()

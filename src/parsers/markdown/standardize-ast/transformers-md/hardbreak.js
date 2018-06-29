e03876ba561b9e3835a5c6940fb0382d87e2e382
Kevin Goslar <kevin.goslar@originate.com>
2018-06-28 22:52:09 -0700

    Support for hardbreak (#459)

---
+++ src/parsers/markdown/standardize-ast/transformers-md/hardbreak.js
@@ -0,0 +1,14 @@
+// @flow
+
+const AstNodeList = require('../../../ast-node-list.js')
+const OpenTagTracker = require('../../helpers/open-tag-tracker.js')
+
+module.exports = function (
+  node: Object,
+  openTags: OpenTagTracker,
+  file: string,
+  line: number
+): AstNodeList {
+  const result = new AstNodeList()
+  return result
+}

757a455ba52148221c044cc1746a9d36acfac8b7
Kevin Goslar <kevin.goslar@gmail.com>
2018-08-29 20:32:09 -0700

    Fix lint

--- src/parsers/markdown/standardize-ast/transformers-md/inline.js
+++ src/parsers/markdown/standardize-ast/transformers-md/inline.js
@@ -1,6 +1,5 @@
 // @flow
 
-const AbsoluteFilePath = require('../../../../domain-model/absolute-file-path.js')
 const AstNodeList = require('../../../ast-node-list.js')
 const OpenTagTracker = require('../../helpers/open-tag-tracker.js')
 

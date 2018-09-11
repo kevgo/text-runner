eff1414ea2a1ca8c912408599d449cc617ce5832
Kevin Goslar <kevin.goslar@gmail.com>
2018-09-10 21:10:00 -0700

    fix

--- src/actions/stop-process.ts
+++ src/actions/stop-process.ts
@@ -1,13 +1,15 @@
-import { ActionArgs } from '../runners/action-args.js'
+import { ActionArgs } from "../runners/action-args.js"
 
-import util from 'util'
-import endChildProcesses = util.promisify(require('end-child-processes'))
+import util from "util"
+import endChildProcessesI from "end-child-processes"
+
+const endChildProcesses = util.promisify(endChildProcessesI)
 
 // Stops the currently running console command.
 export default async function(args: ActionArgs) {
-  args.formatter.name('stopping the long-running process')
+  args.formatter.name("stopping the long-running process")
   if (!global.runningProcess) {
-    throw new Error('No running process found')
+    throw new Error("No running process found")
   }
   global.runningProcess.kill()
   await endChildProcesses()

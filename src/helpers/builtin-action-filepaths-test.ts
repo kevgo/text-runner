8df36fde963b8776849b3271b6d32757afd0b119
Kevin Goslar <kevin.goslar@originate.com>
2018-10-16 03:22:21 +0200

    Windows tests

--- src/helpers/builtin-action-filepaths-test.ts
+++ src/helpers/builtin-action-filepaths-test.ts
@@ -1,9 +1,10 @@
 import { expect } from "chai"
 import builtinActionFilenames from "../../dist/helpers/builtin-action-filepaths"
+import unifixy from "./unifixy"
 
 describe("builtinActionFilenames", function() {
   it("returns the built-in action file paths", function() {
     const result = builtinActionFilenames()
-    expect(result[0]).to.match(/\/dist\/actions\/cd/)
+    expect(unifixy(result[0])).to.match(/\/dist\/actions\/cd/)
   })
 })

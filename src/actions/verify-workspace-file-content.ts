b22f34b1b866e3a6f629743afd362706956b0ff2
Kevin Goslar <kevin.goslar@originate.com>
2018-10-19 19:03:01 -0700

    Extract ReadFile helper (#502)
    * Extract ReadFile helper
    
    * improve
    
    * Fix test
    
    * Async

--- src/actions/verify-workspace-file-content.ts
+++ src/actions/verify-workspace-file-content.ts
@@ -4,23 +4,17 @@ import chalk from "chalk"
 import fs from "fs"
 import jsdiffConsole from "jsdiff-console"
 import path from "path"
+import util from "util"
 
-export default function(args: ActionArgs) {
+const readFileP = util.promisify(fs.readFile)
+
+export default async function(args: ActionArgs) {
   const filePath = args.nodes.textInNodeOfType("strong", "em")
-  const expectedContent = args.nodes.textInNodeOfType("fence", "code")
-  args.formatter.name(`verifying file ${chalk.cyan(filePath)}`)
   const fullPath = path.join(process.cwd(), filePath)
+  args.formatter.name(`verifying file ${chalk.cyan(filePath)}`)
   args.formatter.log(`verify file ${fullPath}`)
-  let actualContent
-  try {
-    actualContent = fs.readFileSync(fullPath, "utf8")
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      throw new Error(`file ${chalk.red(filePath)} not found`)
-    } else {
-      throw err
-    }
-  }
+  const actualContent = await readFile(filePath, fullPath)
+  const expectedContent = args.nodes.textInNodeOfType("fence", "code")
   try {
     jsdiffConsole(actualContent.trim(), expectedContent.trim())
   } catch (err) {
@@ -29,5 +23,18 @@ export default function(args: ActionArgs) {
         err.message
       }`
     )
+  }
+}
+
+async function readFile(filePath: string, fullPath: string): Promise<string> {
+  try {
+    const result = await readFileP(fullPath, "utf8")
+    return result
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error(`file ${chalk.red(filePath)} not found`)
+    } else {
+      throw err
+    }
   }
 }

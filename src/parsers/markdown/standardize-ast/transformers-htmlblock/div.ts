4066b26c9cd1d5060dff3aad141ce6031d7ce5f6
Kevin Goslar <kevin.goslar@originate.com>
2018-11-15 23:16:54 -0500

    DIV tag support (#507)

---
+++ src/parsers/markdown/standardize-ast/transformers-htmlblock/div.ts
@@ -0,0 +1,29 @@
+import AbsoluteFilePath from "../../../../domain-model/absolute-file-path"
+import pretendToUse from "../../../../helpers/pretend-to-use"
+import AstNode from "../../../ast-node"
+import AstNodeList from "../../../ast-node-list"
+import OpenTagTracker from "../../helpers/open-tag-tracker"
+import parseHtmlAttributes from "../../helpers/parse-html-attributes"
+
+const divRegex = /<div([^>]*)>([\s\S]*)<\/div>/m
+
+export default async function transformDiv(
+  node: any,
+  openTags: OpenTagTracker,
+  file: AbsoluteFilePath,
+  line: number
+): Promise<AstNodeList> {
+  const result = new AstNodeList()
+  const divMatch = node.content.match(divRegex)
+  const resultNode = new AstNode({
+    attributes: parseHtmlAttributes(divMatch[1]),
+    content: divMatch[2],
+    file,
+    line,
+    tag: "div",
+    type: "div"
+  })
+  result.pushNode(resultNode)
+  pretendToUse(openTags)
+  return result
+}

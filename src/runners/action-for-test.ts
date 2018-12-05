57a9fea5f600cf9f67f0ca92a75a911b8545e69f
Kevin Goslar <kevin.goslar@originate.com>
2018-12-05 07:38:00 -0800

    Make action-for a class (#538)

--- src/runners/action-for-test.ts
+++ src/runners/action-for-test.ts
@@ -1,12 +1,12 @@
 import { expect } from 'chai'
-import actionFor from '../../dist/runners/action-for'
+import actionRepo from '../../dist/runners/action-repo'
 import { scaffoldActivity } from '../activity-list/activity'
 
 describe('actionFor', function() {
   context('built-in block name given', function() {
     it('returns the matching handler function', function() {
       const activity = scaffoldActivity({ type: 'cd' })
-      const result = actionFor(activity)
+      const result = actionRepo.actionFor(activity)
       expect(result).to.be.a('function')
     })
   })

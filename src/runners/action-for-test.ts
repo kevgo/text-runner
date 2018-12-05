20eda32e9a42fa513430264dc968a3e831ec9f1f
Kevin Goslar <kevin.goslar@gmail.com>
2018-12-05 07:16:08 -0800

    fix

--- src/runners/action-for-test.ts
+++ src/runners/action-for-test.ts
@@ -1,12 +1,12 @@
 import { expect } from 'chai'
-import actionFor from '../../dist/runners/action-for'
 import { scaffoldActivity } from '../activity-list/activity'
+import actionRepo from './action-repo'
 
 describe('actionFor', function() {
   context('built-in block name given', function() {
     it('returns the matching handler function', function() {
       const activity = scaffoldActivity({ type: 'cd' })
-      const result = actionFor(activity)
+      const result = actionRepo.actionFor(activity)
       expect(result).to.be.a('function')
     })
   })

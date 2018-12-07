a1d800fbf1c01f116f9809b6bd83791259c3d5df
Kevin Goslar <kevin.goslar@gmail.com>
2018-12-06 20:32:58 -0800

    Remove sync code

--- src/actions/create-file.ts
+++ src/actions/create-file.ts
@@ -4,13 +4,14 @@ import chalk from 'chalk'
 import fs from 'fs'
 import mkdirp from 'mkdirp'
 import path from 'path'
+import util from 'util'
 
-export default function(args: ActionArgs) {
+export default async function(args: ActionArgs) {
   const filePath = args.nodes.textInNodeOfType('em', 'strong')
   const content = args.nodes.textInNodeOfType('fence', 'code')
   args.formatter.name(`create file ${chalk.cyan(filePath)}`)
   const fullPath = path.join(args.configuration.workspace, filePath)
   args.formatter.log(`create file ${fullPath}`)
   mkdirp.sync(path.dirname(fullPath))
-  fs.writeFileSync(fullPath, content)
+  await util.promisify(fs.writeFile)(fullPath, content)
 }

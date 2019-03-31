b67bd82695ba6887b435c5e7c454fdf28670e807
Kevin Goslar <kevin.goslar@gmail.com>
2019-03-31 21:12:29 +0000

    fix

--- src/helpers/has-directory.ts
+++ src/helpers/has-directory.ts
@@ -1,6 +1,6 @@
 import fs from 'fs-extra'
 
-export default async function hasDirectory(dirname: string): boolean {
+export default async function hasDirectory(dirname: string): Promise<boolean> {
   try {
     const stats = await fs.stat(dirname)
     return stats.isDirectory()

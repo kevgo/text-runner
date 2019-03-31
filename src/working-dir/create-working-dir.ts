bb2794cd4ec8dcec52e6fddddcd9aa033f584b58
Kevin Goslar <kevin.goslar@gmail.com>
2019-03-31 19:36:19 +0000

    Fix

--- src/working-dir/create-working-dir.ts
+++ src/working-dir/create-working-dir.ts
@@ -18,7 +18,7 @@ export default async function createWorkingDir(
   return workingDir
 }
 
-async function getWorkingDirPath(setting): string {
+async function getWorkingDirPath(setting): Promise<string> {
   if (typeof setting === 'string') {
     return setting
   } else if (setting === false) {

0ce51e9807b89a7d9a5df5b12eec3f5b2ac0f1c7
Kevin Goslar <kevin.goslar@originate.com>
2019-03-31 13:32:04 -0500

    Make write-file action async (#576)

--- src/actions/create-file.ts
+++ src/actions/create-file.ts
@@ -1,16 +1,16 @@
 import { ActionArgs } from '../runners/action-args'
 
 import chalk from 'chalk'
-import fs from 'fs'
+import fs from 'fs-extra'
 import mkdirp from 'mkdirp'
 import path from 'path'
 
-export default function(args: ActionArgs) {
+export default async function(args: ActionArgs) {
   const filePath = args.nodes.textInNodeOfType('em', 'strong')
   const content = args.nodes.textInNodeOfType('fence', 'code')
   args.formatter.name(`create file ${chalk.cyan(filePath)}`)
   const fullPath = path.join(args.configuration.workspace, filePath)
   args.formatter.log(`create file ${fullPath}`)
   mkdirp.sync(path.dirname(fullPath))
-  fs.writeFileSync(fullPath, content)
+  await fs.writeFile(fullPath, content)
 }

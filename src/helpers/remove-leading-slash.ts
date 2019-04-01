115539ee0e9152ffe00ff202f0be48ba3411046b
Kevin Goslar <kevin.goslar@gmail.com>
2019-04-01 15:17:25 +0000

    Export removeLeadingSlash

--- src/helpers/remove-leading-slash.ts
+++ src/helpers/remove-leading-slash.ts
@@ -1,4 +1,4 @@
-export default function(text: string): string {
+export function removeLeadingSlash(text: string): string {
   if (!text.startsWith('/') && !text.startsWith('\\')) {
     return text
   }

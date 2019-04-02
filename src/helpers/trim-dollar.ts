b627273fede13a74618aace475d88e4b0d3d48b1
Kevin Goslar <kevin.goslar@originate.com>
2019-04-01 20:42:46 -0500

    Export trimDollar (#603)

--- src/helpers/trim-dollar.ts
+++ src/helpers/trim-dollar.ts
@@ -1,4 +1,4 @@
 // trims the leading dollar from the given command
-export default function(text: string): string {
+export function trimDollar(text: string): string {
   return text.replace(/^\$?\s*/, '')
 }

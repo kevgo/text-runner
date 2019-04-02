293f280c11dc90100ede329ef8ccebaf7689b954
Kevin Goslar <kevin.goslar@originate.com>
2019-04-02 07:11:00 -0500

    Export hasDirectory (#619)

--- src/helpers/has-directory.ts
+++ src/helpers/has-directory.ts
@@ -1,6 +1,6 @@
 import fs from 'fs-extra'
 
-export default async function hasDirectory(dirname: string): Promise<boolean> {
+export async function hasDirectory(dirname: string): Promise<boolean> {
   try {
     const stats = await fs.stat(dirname)
     return stats.isDirectory()

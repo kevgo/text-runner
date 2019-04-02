f9936659cee61941fd4f6077d94c03f15dd82c13
Kevin Goslar <kevin.goslar@gmail.com>
2019-04-02 02:34:06 +0000

    Fix tests

--- src/helpers/unixify-test.ts
+++ src/helpers/unixify-test.ts
@@ -1,14 +1,14 @@
 import { expect } from 'chai'
-import unifixy from './unifixy'
+import { unixify } from './unixify'
 
 describe('unifixy', function() {
   it('converts Windows paths to Unix paths', function() {
-    expect(unifixy('\\foo\\bar\\')).to.equal('/foo/bar/')
+    expect(unixify('\\foo\\bar\\')).to.equal('/foo/bar/')
   })
   it('leaves Unix paths alone', function() {
-    expect(unifixy('/foo/bar/')).to.equal('/foo/bar/')
+    expect(unixify('/foo/bar/')).to.equal('/foo/bar/')
   })
   it('handles mixed path styles', function() {
-    expect(unifixy('/foo\\bar/')).to.equal('/foo/bar/')
+    expect(unixify('/foo\\bar/')).to.equal('/foo/bar/')
   })
 })

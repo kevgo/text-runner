32f85bcaf5184572e84a36a8d473a37c9e8d5de9
Kevin Goslar <kevin.goslar@originate.com>
2019-04-01 20:36:51 -0500

    Export findLinkTargets (#622)

--- src/link-targets/find-link-targets.ts
+++ src/link-targets/find-link-targets.ts
@@ -1,7 +1,7 @@
 import AstNodeList from '../parsers/ast-node-list'
 import LinkTargetList from './link-target-list'
 
-export default function(nodeLists: AstNodeList[]): LinkTargetList {
+export function findLinkTargets(nodeLists: AstNodeList[]): LinkTargetList {
   const linkTargetList = new LinkTargetList()
   for (const nodeList of nodeLists) {
     linkTargetList.addNodeList(nodeList)

8d1a6cd1e624e5ba5e18ff46d16f4cd205178058
Kevin Goslar <kevin.goslar@originate.com>
2019-04-04 13:48:51 -0400

    Export parseHtmlAttributes (#645)
    * Export parseHtmlAttributes
    
    * fix

--- src/parsers/markdown/standardize-ast/transformers-htmlblock/pre.ts
+++ src/parsers/markdown/standardize-ast/transformers-htmlblock/pre.ts
@@ -4,7 +4,7 @@ import { pretendToUse } from '../../../../helpers/pretend-to-use'
 import AstNode from '../../../ast-node'
 import AstNodeList from '../../../ast-node-list'
 import { OpenTagTracker } from '../../helpers/open-tag-tracker'
-import parseHtmlAttributes from '../../helpers/parse-html-attributes'
+import { parseHtmlAttributes } from '../../helpers/parse-html-attributes'
 
 const preRegex = /<pre([^>]*)>([\s\S]*)<\/pre>/m
 

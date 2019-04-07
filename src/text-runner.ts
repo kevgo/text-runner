668eeb7e09134bdbc964d96834c3077555e584ef
Kevin Goslar <kevin.goslar@gmail.com>
2019-04-07 01:50:24 +0000

    Export determineConfigFilename

--- src/text-runner.ts
+++ src/text-runner.ts
@@ -11,7 +11,7 @@ import { runCommand } from './commands/run'
 import { setupCommand } from './commands/setup'
 import { staticCommand } from './commands/static'
 import { versionCommand } from './commands/version'
-import determineConfigFilename from './configuration/determine-config-filename'
+import { determineConfigFilename } from './configuration/determine-config-filename'
 
 // Tests the documentation in the given directory
 export async function textRunner(cmdLineArgs: CliArgTypes): Promise<Error[]> {

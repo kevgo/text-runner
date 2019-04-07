fc59a373519fb7618d2049ffa12487023fee8d58
Kevin Goslar <kevin.goslar@originate.com>
2019-04-07 14:32:56 -0400

    Export StatsCounter (#651)

--- src/commands/static.ts
+++ src/commands/static.ts
@@ -8,7 +8,7 @@ import { getFileNames } from '../finding-files/get-filenames'
 import { findLinkTargets } from '../link-targets/find-link-targets'
 import { readAndParseFile } from '../parsers/read-and-parse-file'
 import { executeParallel } from '../runners/execute-parallel'
-import StatsCounter from '../runners/stats-counter'
+import { StatsCounter } from '../runners/stats-counter'
 import { createWorkingDir } from '../working-dir/create-working-dir'
 
 export async function staticCommand(config: Configuration): Promise<Error[]> {

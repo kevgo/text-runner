5f6aa597c2047e188723f176dbad5a1065424c37
Kevin Goslar <kevin.goslar@originate.com>
2019-04-06 23:24:57 -0400

    Export executeSequential (#657)

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -8,7 +8,7 @@ import { getFileNames } from '../finding-files/get-filenames'
 import { findLinkTargets } from '../link-targets/find-link-targets'
 import { readAndParseFile } from '../parsers/read-and-parse-file'
 import executeParallel from '../runners/execute-parallel'
-import executeSequential from '../runners/execute-sequential'
+import { executeSequential } from '../runners/execute-sequential'
 import StatsCounter from '../runners/stats-counter'
 import { createWorkingDir } from '../working-dir/create-working-dir'
 

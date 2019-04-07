b57d1b49c8c747758123aa591e93d365485524e7
Kevin Goslar <kevin.goslar@originate.com>
2019-04-07 09:39:48 -0400

    Export loadTransformers (#667)

--- src/parsers/markdown/standardize-ast/ast-standardizer.ts
+++ src/parsers/markdown/standardize-ast/ast-standardizer.ts
@@ -4,7 +4,7 @@ import { AstNodeList } from '../../ast-node-list'
 import getHtmlBlockTag from '../helpers/get-html-block-tag'
 import { OpenTagTracker } from '../helpers/open-tag-tracker'
 import { removeHtmlComments } from '../helpers/remove-html-comments'
-import loadTransformers from '../standardize-ast/load-transformers'
+import { loadTransformers } from '../standardize-ast/load-transformers'
 import { TransformerList } from './transformer-list'
 
 // AstStandardizer converts the AST created by Remarkable

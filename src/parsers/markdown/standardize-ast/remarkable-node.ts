30443e9c65d089ac9a96d7ca519af6fe887a1900
Kevin Goslar <kevin.goslar@gmail.com>
2019-04-12 22:39:47 +0000

    types

--- src/parsers/markdown/standardize-ast/remarkable-node.ts
+++ src/parsers/markdown/standardize-ast/remarkable-node.ts
@@ -4,7 +4,7 @@ export class RemarkableNode {
   type: string
   content: string
   attributes: any
-  hLevel: any
+  hLevel: string | undefined
   alt: string | undefined
   src: string | undefined
   href: string | undefined

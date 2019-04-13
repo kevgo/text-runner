d807e6f144402d1e69404150744c5afe8332a34c
Kevin Goslar <kevin.goslar@gmail.com>
2019-04-13 01:21:55 +0000

    Move files

---
+++ src/parsers/markdown/standardize-ast/custom-html-tags/transformers/b.ts
@@ -0,0 +1,31 @@
+import { AbsoluteFilePath } from '../../../../../domain-model/absolute-file-path'
+import { AstNode } from '../../../../ast-node'
+import { AstNodeList } from '../../../../ast-node-list'
+import { OpenTagTracker } from '../../../helpers/open-tag-tracker'
+import { parseHtmlTag } from '../../../helpers/parse-html-tag'
+import { RemarkableNode } from '../../remarkable-node'
+
+export default function transformATag(
+  node: RemarkableNode,
+  openTags: OpenTagTracker,
+  file: AbsoluteFilePath,
+  line: number
+): AstNodeList {
+  const result = new AstNodeList()
+  const [tag, attributes] = parseHtmlTag(
+    node.content,
+    file.platformified(),
+    line
+  )
+  const resultNode = new AstNode({
+    attributes,
+    content: '',
+    file,
+    line,
+    tag,
+    type: 'bold_open'
+  })
+  openTags.add(resultNode)
+  result.pushNode(resultNode)
+  return result
+}

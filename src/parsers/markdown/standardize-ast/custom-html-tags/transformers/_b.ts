f3fe976c978c3c22592d35918667c9d746fc8ec0
Kevin Goslar <kevin.goslar@originate.com>
2019-04-12 20:30:45 -0500

    Move CustomHtmlTagTransformer to subdirectory (#691)
    * Move CustomHtmlTagTransformer to subdirectory
    
    * Move files

---
+++ src/parsers/markdown/standardize-ast/custom-html-tags/transformers/_b.ts
@@ -0,0 +1,27 @@
+import { AbsoluteFilePath } from '../../../../../domain-model/absolute-file-path'
+import { pretendToUse } from '../../../../../helpers/pretend-to-use'
+import { AstNode } from '../../../../ast-node'
+import { AstNodeList } from '../../../../ast-node-list'
+import { OpenTagTracker } from '../../../helpers/open-tag-tracker'
+import { RemarkableNode } from '../../remarkable-node'
+
+export default function transformATag(
+  node: RemarkableNode,
+  openTags: OpenTagTracker,
+  file: AbsoluteFilePath,
+  line: number
+): AstNodeList {
+  const result = new AstNodeList()
+  const openingTag = openTags.popType('bold_open', file, line)
+  const resultNode = new AstNode({
+    attributes: openingTag.attributes,
+    content: '',
+    file,
+    line,
+    tag: '/b',
+    type: 'bold_close'
+  })
+  result.pushNode(resultNode)
+  pretendToUse(node)
+  return result
+}

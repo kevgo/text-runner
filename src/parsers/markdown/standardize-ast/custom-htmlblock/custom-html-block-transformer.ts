64052573b7eedbdf75c7c9621cccbe660264f9d0
Kevin Goslar <kevin.goslar@originate.com>
2019-04-13 19:15:10 -0500

    Move custom HtmlBlock transformer into subdir (#698)

---
+++ src/parsers/markdown/standardize-ast/custom-htmlblock/custom-html-block-transformer.ts
@@ -0,0 +1,46 @@
+import { AbsoluteFilePath } from '../../../../domain-model/absolute-file-path'
+import { UnprintedUserError } from '../../../../errors/unprinted-user-error'
+import { AstNodeList } from '../../../ast-node-list'
+import { getHtmlBlockTag } from '../../helpers/get-html-block-tag'
+import { OpenTagTracker } from '../../helpers/open-tag-tracker'
+import { removeHtmlComments } from '../../helpers/remove-html-comments'
+import { loadLocalTransformers } from '../load-local-transformers'
+import { RemarkableNode } from '../remarkable-node'
+import { TransformerList } from '../transformer-list'
+
+export class CustomHtmlBlockTransformer {
+  openTags: OpenTagTracker
+  transformers: TransformerList
+
+  constructor(openTags: OpenTagTracker) {
+    this.openTags = openTags
+    this.transformers = {}
+  }
+
+  canTransform(node: RemarkableNode): boolean {
+    return node.type === 'htmlblock'
+  }
+
+  async loadTransformers() {
+    this.transformers = await loadLocalTransformers(__dirname)
+  }
+
+  async transform(
+    node: RemarkableNode,
+    file: AbsoluteFilePath,
+    line: number
+  ): Promise<AstNodeList> {
+    const cleanedContent = removeHtmlComments(node.content)
+    const tagName = getHtmlBlockTag(cleanedContent, file, line)
+    const transformer = this.transformers[tagName]
+    if (!transformer) {
+      throw new UnprintedUserError(
+        `Unknown HTML block: '${tagName}'`,
+        file.platformified(),
+        line
+      )
+    }
+    const transformed = await transformer(node, this.openTags, file, line)
+    return transformed
+  }
+}

f196de7b6ce13eff83b56f98686d144cd5c14a8e
Kevin Goslar <kevin.goslar@originate.com>
2019-04-17 09:45:53 -0400

    Rename test (#711)

---
+++ src/runners/action-repo-test.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import { actionRepo } from '../../dist/runners/action-repo'
+import { scaffoldActivity } from '../activity-list/activity'
+
+describe('actionRepo', function() {
+  describe('actionFor', function() {
+    context('built-in block name given', function() {
+      it('returns the matching handler function', function() {
+        const activity = scaffoldActivity({ type: 'cd' })
+        const result = actionRepo.actionFor(activity)
+        expect(result).to.be.a('function')
+      })
+    })
+  })
+})

f03541a0728d8efb32e7db218f61b3baca6f1c92
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-13 18:03:28 +0800

    Fix windows issue

--- features/support/env.ts
+++ features/support/env.ts
@@ -2,6 +2,8 @@ import { After, Before, setDefaultTimeout } from 'cucumber'
 import { endChildProcesses } from 'end-child-processes'
 import fs from 'fs-extra'
 import path from 'path'
+import rimraf from 'rimraf'
+import util from 'util'
 
 // need such a high timeout because test coverage takes time to start up
 setDefaultTimeout(30000)
@@ -26,7 +28,9 @@ After(async function(scenario) {
   if (scenario.result.status === 'failed') {
     console.log('\ntest artifacts are located in', this.rootDir)
   } else {
-    await fs.remove(this.rootDir)
+    // NOTE: need rimraf here because Windows requires to retry this for a few times
+    const rimrafp = util.promisify(rimraf)
+    await rimrafp(this.rootDir, { maxBusyTries: 20 })
   }
 })
 

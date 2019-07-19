7fee0b7c0e9ee236ebd6a7773320d8478d97cfdb
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-19 17:38:05 -0500

    String literal LinkTargetTypes (#760)

--- src/link-targets/link-target.ts
+++ src/link-targets/link-target.ts
@@ -1,5 +1,11 @@
+import { LinkTargetTypes } from "./link-target-types"
+
+/**
+ * LinkTarget is a position in a Markdown file that links can point to:
+ * headers or anchors
+ */
 export interface LinkTarget {
-  type: string
+  type: LinkTargetTypes
   name: string
   text?: string
   level?: number

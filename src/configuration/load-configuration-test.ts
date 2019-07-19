9abb694a4537438fee3dad83dc1317a2b94f4f9f
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-19 16:33:57 -0500

    Rename determineConfiguration (#755)
    * Rename determineConfiguration
    
    * fix

--- src/configuration/load-configuration-test.ts
+++ src/configuration/load-configuration-test.ts
@@ -2,11 +2,11 @@ import { expect } from 'chai'
 import fs from 'fs-extra'
 import path from 'path'
 import tmp from 'tmp-promise'
-import { loadConfiguration } from './load-configuration'
+import { determineConfiguration } from './determine-configuration'
 
 describe('loadConfiguration', function() {
   it('returns default values if no config file is given', function() {
-    const result = loadConfiguration({}, { command: '' })
+    const result = determineConfiguration({}, { command: '' })
     expect(result.fileGlob).to.equal('**/*.md')
   })
 
@@ -15,7 +15,7 @@ describe('loadConfiguration', function() {
       this.configDir = await tmp.dir()
       this.configFilePath = path.join(this.configDir.name, 'text-run.yml')
       await fs.writeFile(this.configFilePath, "files: '*.md'")
-      const result = loadConfiguration({}, { command: '' })
+      const result = determineConfiguration({}, { command: '' })
       expect(result.fileGlob).to.equal('*.md')
     })
   })

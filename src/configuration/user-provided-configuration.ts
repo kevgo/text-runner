b8a682233ce623c9b961cf61f543370ff8d99bed
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-19 14:44:47 -0500

    cleanup

--- src/configuration/user-provided-configuration.ts
+++ src/configuration/user-provided-configuration.ts
@@ -10,7 +10,7 @@ export interface UserProvidedConfiguration {
   command?: string // the text-runner command to run
   configFileName?: string // name of the config file to use
   defaultFile?: string
-  exclude?: string[]
+  exclude?: string | string[]
   fileGlob?: string // files to test
   formatterName?: string // name of the formatter to use
   keepTmp?: boolean

1f5fd012ea2a00510850851e1bd432c6a8a4c4f2
Kevin Goslar <kevin.goslar@gmail.com>
2019-07-26 11:55:51 -0500

    Find Markdown files asynchronously

--- src/filesystem/all-markdown-files.ts
+++ src/filesystem/all-markdown-files.ts
@@ -1,13 +1,15 @@
-import glob from "glob"
+import glob from "glob-promise"
 import { AbsoluteFilePath } from "./absolute-file-path"
 
 /**
  * Returns all the markdown files in the current working directory
  * @param fileGlob
  */
-export function allMarkdownFiles(fileGlob: string): AbsoluteFilePath[] {
-  return glob
-    .sync(fileGlob)
+export async function allMarkdownFiles(
+  fileGlob: string
+): Promise<AbsoluteFilePath[]> {
+  const files = await glob(fileGlob)
+  return files
     .filter(file => !file.includes("node_modules"))
     .sort()
     .map(file => new AbsoluteFilePath(file))
--- src/filesystem/markdown-files-in-dir.ts
+++ src/filesystem/markdown-files-in-dir.ts
@@ -1,11 +1,13 @@
-import glob from "glob"
+import glob from "glob-promise"
 import { AbsoluteFilePath } from "./absolute-file-path"
 
 /**
  * Returns all the markdown files in this directory and its children
  */
-export function markdownFilesInDir(dirName: string): AbsoluteFilePath[] {
-  const files = glob.sync(`${dirName}/**/*.md`)
+export async function markdownFilesInDir(
+  dirName: string
+): Promise<AbsoluteFilePath[]> {
+  const files = await glob(`${dirName}/**/*.md`)
   return files
     .filter(file => !file.includes("node_modules"))
     .sort()

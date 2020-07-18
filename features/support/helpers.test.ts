fdd5b631567c3cecae8ea3459d2be6140d28faaa
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-18 15:49:25 -0500

    progress

--- features/support/helpers.test.ts
+++ features/support/helpers.test.ts
@@ -1,21 +1,30 @@
-import { makeFullPath } from "./helpers"
+import * as helpers from "./helpers"
 import { assert } from "chai"
 
 suite("makeFullPath", function () {
   test("with text-run command on Linux", function () {
-    const have = makeFullPath("text-run foo", "linux")
+    const have = helpers.makeFullPath("text-run foo", "linux")
     assert.match(have, /.+text-runner\/bin\/text-run foo/)
   })
   test("without text-run command on Linux", function () {
-    const have = makeFullPath("run", "linux")
+    const have = helpers.makeFullPath("run", "linux")
     assert.match(have, /.+text-runner\/bin\/text-run run/)
   })
   test("with text-run command on Linux", function () {
-    const have = makeFullPath("text-run foo", "win32")
+    const have = helpers.makeFullPath("text-run foo", "win32")
     assert.match(have, /.+text-runner\/bin\/text-run.cmd foo/)
   })
   test("without text-run command on Linux", function () {
-    const have = makeFullPath("run", "win32")
+    const have = helpers.makeFullPath("run", "win32")
     assert.match(have, /.+text-runner\/bin\/text-run.cmd run/)
+  })
+})
+
+suite("standardizePath", function () {
+  test("unix path", function () {
+    assert.equal(helpers.standardizePath("foo/bar"), "foo/bar")
+  })
+  test("windows path", function () {
+    assert.equal(helpers.standardizePath("foo\\bar"), "foo/bar")
   })
 })

61022a917b1e9683a1bdbe7c9d04b7786968b7aa
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-18 16:09:02 -0500

    fix

--- features/support/helpers.test.ts
+++ features/support/helpers.test.ts
@@ -4,19 +4,19 @@ import { assert } from "chai"
 suite("makeFullPath", function () {
   test("with text-run command on Linux", function () {
     const have = helpers.makeFullPath("text-run foo", "linux")
-    assert.match(have, /.+\/text-runner\/bin\/text-run foo$/)
+    assert.match(have, /.+\/bin\/text-run foo$/)
   })
   test("without text-run command on Linux", function () {
     const have = helpers.makeFullPath("run", "linux")
-    assert.match(have, /.+\/text-runner\/bin\/text-run run$/)
+    assert.match(have, /.+\/bin\/text-run run$/)
   })
   test("with text-run command on Linux", function () {
     const have = helpers.makeFullPath("text-run foo", "win32")
-    assert.match(have, /.+\/text-runner\/bin\/text-run.cmd foo$/)
+    assert.match(have, /.+\/bin\/text-run.cmd foo$/)
   })
   test("without text-run command on Linux", function () {
     const have = helpers.makeFullPath("run", "win32")
-    assert.match(have, /.+\/text-runner\/bin\/text-run.cmd run$/)
+    assert.match(have, /.+\/bin\/text-run.cmd run$/)
   })
 })
 

4b79c06d04a637b227e0583f25f3592b73ef91cc
Kevin Goslar <kevin.goslar@gmail.com>
2020-07-29 19:04:35 -0500

    progress

--- textrun-action/src/name-full.ts
+++ textrun-action/src/name-full.ts
@@ -5,7 +5,8 @@ export function nameFull(args: ActionArgs) {
   const want = args.nodes.text()
   const wantStd = actionName(want)
   const pkgJson = require(path.join(args.configuration.sourceDir, "package.json"))
-  const allNames = Object.keys(pkgJson.textrunActions)
+  const main = require(path.join(args.configuration.sourceDir, pkgJson.main))
+  const allNames = Object.keys(main.textrunActions)
   const allNamesStd = allNames.map(actionName)
   if (!allNamesStd.includes(wantStd)) {
     throw new Error(`This module does not export action "${want}. Found [${allNames.join(", ")}] `)

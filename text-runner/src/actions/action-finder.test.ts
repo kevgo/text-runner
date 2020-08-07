3fd25f404bc974aadf3a24c36fe6e61ba2614c5d
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-07 18:35:57 -0500

    fixes

--- text-runner/src/actions/action-finder.test.ts
+++ text-runner/src/actions/action-finder.test.ts
@@ -15,15 +15,8 @@ suite("actionFinder", function () {
   suite("loadCustomActions", function () {
     test("with text-run folder of this codebase", function () {
       const result = loadCustomActions(path.join(__dirname, "..", "..", "..", "documentation", "text-run"))
-      assert.typeOf(result["cd-into-empty-tmp-folder"], "function")
-      assert.typeOf(result["cd-workspace"], "function")
-      assert.typeOf(result["create-markdown-file"], "function")
-      assert.typeOf(result["run-in-textrunner"], "function")
-      assert.typeOf(result["run-textrun"], "function")
       assert.typeOf(result["verify-ast-node-attributes"], "function")
       assert.typeOf(result["verify-handler-args"], "function")
-      assert.typeOf(result["verify-make-command"], "function")
-      assert.typeOf(result["verify-searcher-methods"], "function")
     })
   })
 })

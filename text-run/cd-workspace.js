7debc79deee7c58a70bd4b47a28dedc80955aea2
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-07 17:24:43 -0500

    cleanup

--- text-run/cd-workspace.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function cdWorkspace(args) {
-  args.log("cd into the workspace")
-  process.chdir(args.configuration.workspace)
-}
--- text-run/is-yarn-workspace.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function (args) {
-  const packageDefinition = require("../package.json")
-  if (packageDefinition.private === undefined) {
-    throw new Error("this codebase is not a Yarn workspace")
-  }
-}

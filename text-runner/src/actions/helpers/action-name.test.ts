1c0cd7bbb8cd9870b6f26d9fe554f3b31c0cb0a3
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-11 11:41:28 -0500

    cleanup

--- text-runner/src/actions/helpers/action-name.test.ts
+++ text-runner/src/actions/helpers/action-name.test.ts
@@ -2,5 +2,5 @@ import { assert } from "chai"
 import { actionName } from "./action-name"
 
 test("getActionName()", function () {
-  assert.equal(actionName("/d/text-runner/text-run/cdBack.js"), "cd-back")
+  assert.equal(actionName("/users/foo/text-runner/text-run/cdBack.js"), "cd-back")
 })

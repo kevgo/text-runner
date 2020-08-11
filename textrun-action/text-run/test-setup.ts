b286bd3c4c257fd29eba638c8a6f3d4afda2ed2b
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-11 11:59:19 -0500

    cleanup

--- textrun-action/text-run/test-setup.ts
+++ textrun-action/text-run/test-setup.ts
@@ -4,12 +4,11 @@ import path from "path"
 
 export default async function testSetup(action: ActionArgs) {
   const codeBlocks = action.nodes.getNodesOfTypes("code")
-  if (codeBlocks.length !== 3) {
-    throw new Error(`Expected 3 code blocks, got ${codeBlocks.length}`)
+  if (codeBlocks.length !== 2) {
+    throw new Error(`Expected 2 code blocks, got ${codeBlocks.length}`)
   }
   const pkgName = action.nodes.getNodesFor(codeBlocks[0]).text()
   const action1 = action.nodes.getNodesFor(codeBlocks[1]).text()
-  const action2 = action.nodes.getNodesFor(codeBlocks[2]).text()
   await fs.writeFile(
     path.join(action.configuration.workspace, "package.json"),
     `\
@@ -24,8 +23,7 @@ export default async function testSetup(action: ActionArgs) {
     `\
 module.exports = {
   textrunActions: {
-    "${action1}": function() { console.log(1) },
-    "${action2}": function() {}
+    "${action1}": function() { console.log(1) }
   }
 }`
   )

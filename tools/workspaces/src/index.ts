7953c6fa4c60f251a5d7f0493002a7477bf6546c
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-13 13:20:55 -0500

    progress

--- tools/workspaces/src/index.ts
+++ tools/workspaces/src/index.ts
@@ -2,11 +2,12 @@ import * as child_process from "child_process"
 import * as color from "colorette"
 import * as os from "os"
 import * as fs from "fs"
-import { YarnReader } from "./yarn-reader"
+import { YarnReader, YarnInfo } from "./yarn-reader"
 import { WorkspaceTagger } from "./workspace-tagger"
 
 // learn workspace dependency info
-const yarnInfo = new YarnReader(workspaceInfo())
+const file = JSON.parse(child_process.execSync("yarn workspaces --silent info", { encoding: "utf-8" }))
+const yarnInfo = new YarnReader(file)
 
 // determine the provided workspaces
 const files = fs.readFileSync(0, "utf-8").split(os.EOL)
@@ -20,26 +21,16 @@ console.error("changed workspaces:", providedWorkspaces)
 
 // determine all affected workspaces
 for (const workspace of providedWorkspaces) {
-  const upstreams = yarnInfo.upstreamsFor(workspace)
-  for (const upstream of upstreams) {
-    if (!taggedWorkspaces.isTagged(upstream)) {
-      console.error(`${color.cyan(workspace)} is an upstream of ${color.cyan(upstream)}`)
+  const downstreams = yarnInfo.downstreamsFor(workspace)
+  for (const downstream of downstreams) {
+    if (!taggedWorkspaces.isTagged(downstream)) {
+      console.error(`${color.cyan(workspace)} is a downstream of ${color.cyan(downstream)}`)
     }
   }
-  taggedWorkspaces.tagMany(upstreams)
+  taggedWorkspaces.tagMany(downstreams)
 }
 const affectedWorkspaces = taggedWorkspaces.tagged()
 console.error("all affected workspaces:", affectedWorkspaces)
 
 // write to STDOUT
 console.log(affectedWorkspaces.join(os.EOL))
-
-/** Loads Yarn workspace information */
-function workspaceInfo(): Map<string, string[]> {
-  const result = new Map()
-  const yarnInfo = JSON.parse(child_process.execSync("yarn workspaces --silent info", { encoding: "utf-8" }))
-  for (const key of Object.keys(yarnInfo)) {
-    result.set(yarnInfo[key].location, yarnInfo[key].workspaceDependencies)
-  }
-  return result
-}
--- tools/workspaces/src/root-folder.ts
+++
@@ -1,6 +0,0 @@
-import * as path from "path"
-
-/** provides the top-level folder containing the given filename */
-export function rootFolder(file: string): string {
-  return path.dirname(file).split(path.sep)[0]
-}

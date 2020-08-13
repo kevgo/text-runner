c614479be5fd6d9b298cf74634df8998ad09a971
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-13 14:58:14 -0500

    polish

--- tools/workspaces/src/cli.ts
+++ tools/workspaces/src/cli.ts
@@ -3,7 +3,7 @@ import { workspaces } from "./workspaces"
 
 function noLog(message?: any, ...optionalParams: any[]) {}
 
-const file = JSON.parse(child_process.execSync("yarn workspaces --silent info", { encoding: "utf-8" }))
+const yarnOutput = JSON.parse(child_process.execSync("yarn workspaces --silent info", { encoding: "utf-8" }))
 const param = process.argv[process.argv.length - 1]
 const log = param === "--log" ? console.error : noLog
-workspaces(file, log)
+workspaces(yarnOutput, log)

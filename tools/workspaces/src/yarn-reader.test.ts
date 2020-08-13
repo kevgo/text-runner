ee446eb33c3d50b7335c8284929efd17bd76c80b
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-13 13:09:44 -0500

    progress

---
+++ tools/workspaces/src/yarn-reader.test.ts
@@ -0,0 +1,55 @@
+import { YarnReader, YarnInfo } from "./yarn-reader"
+import { strict as assert } from "assert"
+
+suite("YarnReader", function () {
+  test("downstreamsFor normal", function () {
+    const yarnOutput: YarnInfo = {
+      a: {
+        location: "a path",
+        workspaceDependencies: ["b"],
+      },
+      b: {
+        location: "b path",
+        workspaceDependencies: ["c"],
+      },
+      c: {
+        location: "c path",
+        workspaceDependencies: [],
+      },
+    }
+    const uf = new YarnReader(yarnOutput)
+    assert.deepEqual(uf.downstreamsFor("a path"), [])
+    assert.deepEqual(uf.downstreamsFor("b path"), ["a path"])
+    assert.deepEqual(uf.downstreamsFor("c path"), ["a path", "b path"])
+    assert.throws(() => uf.downstreamsFor("zonk"), "unregistered workspace: zonk")
+  })
+  test("downstreamsFor with cyclical dependency", function () {
+    const yarnOutput: YarnInfo = {
+      a: {
+        location: "a path",
+        workspaceDependencies: ["b"],
+      },
+      b: {
+        location: "b path",
+        workspaceDependencies: ["a"],
+      },
+    }
+    const uf = new YarnReader(yarnOutput)
+    assert.deepEqual(uf.downstreamsFor("a path"), ["b path"])
+    assert.deepEqual(uf.downstreamsFor("b path"), ["a path"])
+  })
+  test("pathsFor", function () {
+    const yarnOutput: YarnInfo = {
+      a: {
+        location: "a path",
+        workspaceDependencies: [],
+      },
+      b: {
+        location: "b path",
+        workspaceDependencies: [],
+      },
+    }
+    const uf = new YarnReader(yarnOutput)
+    assert.deepEqual(uf.pathsFor(["b", "a"]), ["a path", "b path"])
+  })
+})

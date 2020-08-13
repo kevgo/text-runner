6cf003784adf254a3f39874d306a90ecee1553ef
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-13 15:27:40 -0500

    Minimist

--- tools/workspaces/src/cli.ts
+++ tools/workspaces/src/cli.ts
@@ -1,9 +1,10 @@
 import * as child_process from "child_process"
 import { workspaces } from "./workspaces"
+import * as minimist from "minimist"
 
 function noLog(message?: any, ...optionalParams: any[]) {}
 
 const yarnOutput = JSON.parse(child_process.execSync("yarn workspaces --silent info", { encoding: "utf-8" }))
-const param = process.argv[process.argv.length - 1]
-const log = param === "--log" ? console.error : noLog
+const args = minimist(process.argv.slice(2))
+const log = args.log ? console.error : noLog
 workspaces(yarnOutput, log)

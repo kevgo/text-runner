f49ed121fa06cb0b65faa15a44559302e11ff5f8
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-13 22:35:33 -0500

    Fi

--- text-runner/features/support/when-steps.ts
+++ text-runner/features/support/when-steps.ts
@@ -6,7 +6,7 @@ When(/^(trying to execute|executing) the "([^"]+)" example$/, { timeout: 100000 
   exampleName
 ) {
   const expectError = determineExpectError(tryingText)
-  const examplePath = path.join(process.cwd(), "examples", exampleName)
+  const examplePath = path.join(process.cwd(), "..", "examples", exampleName)
   await this.execute({ command: "run", expectError, cwd: examplePath })
   finish(expectError, this.process && (this.process.error || this.process.exitCode))
 })

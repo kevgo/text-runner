3385af80fe31a46cac970216363515f36e4b88fe
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-15 14:54:04 -0500

    progress

--- textrun-workspace/src/index.ts
+++ textrun-workspace/src/index.ts
@@ -1,11 +1,13 @@
 import { appendFile } from "./actions/append-file"
 import { cd } from "./actions/cd"
 import { directory } from "./actions/directory"
+import { fileContent } from "./actions/file-content"
 import { mkdir } from "./actions/mkdir"
 
 export const textrunActions = {
   appendFile,
   cd,
   directory,
+  fileContent,
   mkdir,
 }

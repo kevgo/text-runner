50afd1ab44acffbf46afc7dc6f18dacdc9c0441d
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-15 08:02:45 -0500

    Move support folder

--- text-runner/cucumber.js
+++ text-runner/cucumber.js
@@ -1,3 +1,3 @@
 module.exports = {
-  default: "--require-module ts-node/register --require 'features/support/!(*.test).ts' --fail-fast",
+  default: "--require-module ts-node/register --require '../shared/features/support/!(*.test).ts' --fail-fast",
 }

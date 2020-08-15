bf9fcda1182fb25911583bb492afb8b8d6cf96be
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-15 09:04:00 -0500

    progress

--- textrun-shell/cucumber.js
+++ textrun-shell/cucumber.js
@@ -1,3 +1,3 @@
 module.exports = {
-  default: "--require-module ts-node/register --require '../text-runner/features/support/!(*.test).ts' --fail-fast",
+  default: "--require '../shared/features/support/dist/!(*.test).js' --fail-fast",
 }

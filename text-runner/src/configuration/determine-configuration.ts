a2db73c58c9498b9035592d0023bbd289c99d7ff
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-24 08:51:00 -0500

    Remove Configuration.actions

--- text-runner/src/configuration/determine-configuration.ts
+++ text-runner/src/configuration/determine-configuration.ts
@@ -4,7 +4,6 @@ import { Configuration } from "./types/configuration"
 import { UserProvidedConfiguration } from "./types/user-provided-configuration"
 
 const defaultValues: Configuration = {
-  actions: {},
   regionMarker: "type",
   defaultFile: "",
   exclude: [],

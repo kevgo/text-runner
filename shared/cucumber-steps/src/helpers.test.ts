2f78c81deb82d0442fb4b9ae5cbcc65a286bac38
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-27 08:16:16 -0500

    Remove unused function (#1073)

--- shared/cucumber-steps/src/helpers.test.ts
+++ shared/cucumber-steps/src/helpers.test.ts
@@ -2,24 +2,6 @@ import * as helpers from "./helpers"
 import { assert } from "chai"
 import * as path from "path"
 
-suite("globalTextRunPath", function () {
-  test("on unix", function () {
-    if (process.platform === "win32") {
-      return
-    }
-    const want = path.join(process.cwd(), "..", "..", "text-runner", "bin", "text-run")
-    const have = helpers.globalTextRunPath("linux")
-    assert.equal(have, want)
-  })
-  test("on windows", function () {
-    if (process.platform !== "win32") {
-      return
-    }
-    const want = "c:\\text-runner\\documentation\\examples\\bash\\node_modules\\.bin\\text-run.cmd"
-    assert.equal(helpers.globalTextRunPath("win32"), want)
-  })
-})
-
 suite("makeFullPath", function () {
   const linuxTests = {
     "text-run foo": /.+\/bin\/text-run foo$/,

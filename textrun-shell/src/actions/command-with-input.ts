da39df5936bc34444db1f2675351bf1d2bb2fa39
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-27 14:45:27 -0500

    Optimize command parsing

--- textrun-shell/src/actions/command-with-input.ts
+++ textrun-shell/src/actions/command-with-input.ts
@@ -45,7 +45,6 @@ export async function commandWithInput(action: ActionArgs) {
 }
 
 async function enter(processor: ObservableProcess, input: ProcessInput) {
-  // TODO: reduce redundancy
   processor.stdin.write(input.input + "\n")
   if (input.textToWait) {
     await processor.stdout.waitForText(input.textToWait)

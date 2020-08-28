8998bc7e5bed6fff18498ca59981731c4996a658
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-28 13:04:20 -0500

    progress

--- text-runner/src/configuration/merge-configurations.test.ts
+++ text-runner/src/configuration/merge-configurations.test.ts
@@ -1,24 +1,32 @@
 import { assert } from "chai"
 import { mergeConfigurations } from "./merge-configurations"
 import { UserProvidedConfiguration } from "./types/user-provided-configuration"
+import { defaultValues } from "./determine-configuration"
 
-test("mergeConfigurations()", function () {
-  const cmdlineArgs: UserProvidedConfiguration = {
-    fileGlob: "1.md",
-    offline: false,
-  }
-  const configFileData: UserProvidedConfiguration = {
-    command: "run",
-    fileGlob: "**/*.md",
-    offline: true,
-  }
-  const defaultValues: UserProvidedConfiguration = {
-    fileGlob: "*.md",
-  }
-  const result = mergeConfigurations(cmdlineArgs, configFileData, defaultValues)
-  assert.deepEqual(result, {
-    command: "run",
-    fileGlob: "1.md",
-    offline: false,
+suite("mergeConfigurations()", function () {
+  test("no CLI args and config file data given", function () {
+    const result = mergeConfigurations({}, {}, defaultValues)
+    assert.deepEqual(result, defaultValues)
+  })
+
+  test("xxx", function () {
+    const cmdlineArgs: UserProvidedConfiguration = {
+      fileGlob: "1.md",
+      offline: false,
+    }
+    const configFileData: UserProvidedConfiguration = {
+      command: "run",
+      fileGlob: "**/*.md",
+      offline: true,
+    }
+    const defaultValues: UserProvidedConfiguration = {
+      fileGlob: "*.md",
+    }
+    const result = mergeConfigurations(cmdlineArgs, configFileData, defaultValues)
+    assert.deepEqual(result, {
+      command: "run",
+      fileGlob: "1.md",
+      offline: false,
+    })
   })
 })

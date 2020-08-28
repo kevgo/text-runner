d21b869ba90921c4eaaeba639a887b2bb6574137
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-28 07:27:44 -0500

    progress

--- textrun-extension/src/actions/run-textrunner.ts
+++ textrun-extension/src/actions/run-textrunner.ts
@@ -10,7 +10,6 @@ export async function runTextrunner(action: ActionArgs) {
   var textRunPath = path.join(action.configuration.sourceDir, "..", "text-runner", "bin", "text-run")
   if (process.platform === "win32") textRunPath += ".cmd"
   const trArgs = callArgs(textRunPath, process.platform)
-  trArgs[trArgs.length - 1] += " --keep-workspace"
   const processor = createObservableProcess(trArgs, { cwd: action.configuration.workspace })
   await processor.waitForEnd()
   action.log(processor.output.fullText())

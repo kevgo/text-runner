e880059b801e692367672c5f620302260b32621e
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-28 13:12:11 -0500

    conf

--- text-runner/src/configuration/merge-configurations.test.ts
+++ text-runner/src/configuration/merge-configurations.test.ts
@@ -2,14 +2,35 @@ import { assert } from "chai"
 import { mergeConfigurations } from "./merge-configurations"
 import { UserProvidedConfiguration } from "./types/user-provided-configuration"
 import { defaultValues } from "./determine-configuration"
+import { Configuration } from "./types/configuration"
+import { Publications } from "./publications/publications"
 
 suite("mergeConfigurations()", function () {
   test("no CLI args and config file data given", function () {
-    const result = mergeConfigurations({}, {}, defaultValues)
-    assert.deepEqual(result, defaultValues)
+    const have = mergeConfigurations({}, {}, defaultValues)
+    assert.deepEqual(have, defaultValues)
+  })
+  test("config file data given", function () {
+    const configFileData: UserProvidedConfiguration = {
+      exclude: "1.md",
+    }
+    const have = mergeConfigurations({}, configFileData, defaultValues)
+    const want: Configuration = {
+      defaultFile: "",
+      exclude: "1.md",
+      fileGlob: "**/*.md",
+      formatterName: "detailed",
+      offline: false,
+      publications: new Publications(),
+      regionMarker: "type",
+      sourceDir: process.cwd(),
+      useSystemTempDirectory: false,
+      workspace: "",
+    }
+    assert.deepEqual(have, want)
   })
 
-  test("xxx", function () {
+  test("complex example", function () {
     const cmdlineArgs: UserProvidedConfiguration = {
       fileGlob: "1.md",
       offline: false,

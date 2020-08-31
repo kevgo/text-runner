efc4f749aec6497f792adc388ed185eca08c7ecb
Kevin Goslar <kevin.goslar@gmail.com>
2020-08-31 16:08:30 -0500

    Simplify SilentFormatter (#1140)

--- text-runner/src/formatters/silent-formatter.ts
+++ text-runner/src/formatters/silent-formatter.ts
@@ -1,29 +1,15 @@
-import * as color from "colorette"
-import * as path from "path"
 import { Activity } from "../activity-list/types/activity"
 import { Configuration } from "../configuration/types/configuration"
-import { printCodeFrame } from "../helpers/print-code-frame"
 import { Formatter } from "./types/formatter"
 import { StatsCounter } from "../runners/helpers/stats-counter"
 
 /** A completely minimalistic formatter, prints nothing in the happy path */
 export class SilentFormatter implements Formatter {
-  /** Text-Runner configuration */
-  private readonly configuration: Configuration
-
   // @ts-ignore: ignore unused variable
-  constructor(stepCount: number, configuration: Configuration) {
-    this.configuration = configuration
-  }
+  constructor(stepCount: number, configuration: Configuration) {}
 
   // @ts-ignore: okay to not use parameters here
-  failed(activity: Activity, stepName: string, err: Error, output: string) {
-    console.log()
-    console.log(color.dim(output))
-    process.stdout.write(color.red(`${activity.file.platformified()}:${activity.line} -- `))
-    console.log(err.message)
-    printCodeFrame(console.log, path.join(this.configuration.sourceDir, activity.file.platformified()), activity.line)
-  }
+  failed(activity: Activity, stepName: string, err: Error, output: string) {}
 
   // @ts-ignore: okay to not use parameters here
   skipped(activity: Activity, stepName: string, output: string) {}

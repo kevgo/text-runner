7e13c9453fd0aa5a53b923949970bd4dc900fbdb
Kevin Goslar <kevin.goslar@gmail.com>
2020-09-04 09:57:51 -0500

    Provide more guidance on workspace errors

--- textrun-workspace/src/actions/new-file.ts
+++ textrun-workspace/src/actions/new-file.ts
@@ -1,11 +1,23 @@
 import * as color from "colorette"
 import * as fs from "fs-extra"
 import * as path from "path"
-import { ActionArgs } from "text-runner"
+import { ActionArgs, UserError } from "text-runner"
 
 export async function newFile(action: ActionArgs) {
-  const filePath = action.region.textInNodeOfType("em", "strong")
-  const content = action.region.textInNodeOfType("fence", "code")
+  let filePath
+  try {
+    filePath = action.region.textInNodeOfType("em", "strong")
+  } catch (e) {
+    const guidance = "Cannot determine the name of the file to create.\n" + e.guidance
+    throw new UserError(e.message, guidance)
+  }
+  let content
+  try {
+    content = action.region.textInNodeOfType("fence", "code")
+  } catch (e) {
+    const guidance = "Cannot determine the content of the file to create.\n" + e.guidance
+    throw new UserError(e.message, guidance)
+  }
   action.name(`create file ${color.cyan(filePath)}`)
   const fullPath = path.join(action.configuration.workspace, filePath)
   action.log(`create file ${filePath}`)

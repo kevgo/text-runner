7db6a801bd74a01ec95a605edbca5aeb2736777c
Kevin Goslar <kevin.goslar@gmail.com>
2020-09-04 22:16:55 -0500

    doc

--- text-runner/src/commands/help.ts
+++ text-runner/src/commands/help.ts
@@ -16,20 +16,20 @@ ${error || ""}
 USAGE: ${color.bold("text-run [<options>] <command>")}
 
 COMMANDS
-  ${color.bold("run")} [<filename>]      runs all tests on the given file/folder or entire documentation
-  ${color.bold("dynamic")} [<filename>]  runs only the programmatic tests, skips checking links
-  ${color.bold("static")} [<filename>]   checks only the links, skips programmatic tests
+  ${color.bold("run")} [<filename>]         runs all tests on the given file/folder or entire documentation
+  ${color.bold("dynamic")} [<filename>]     runs only the programmatic tests, skips checking links
+  ${color.bold("static")} [<filename>]      checks only the links, skips programmatic tests
 
-  ${color.bold("setup")}                 creates an example configuration file
-  ${color.bold("scaffold")} <filename>   scaffolds a new region type handler
-  ${color.bold("unused")} <filename>     shows unused custom activities
+  ${color.bold("setup")}                    creates an example configuration file
+  ${color.bold("scaffold")} [--ts] <name>   scaffolds a new region type handler (--ts = in TypeScript)
+  ${color.bold("unused")} <filename>        shows unused custom activities
 
-  ${color.bold("help")}                  shows this help screen
-  ${color.bold("version")}               shows the currently installed version
-  ${color.bold("debug")}                 shows debug data
+  ${color.bold("help")}                     shows this help screen
+  ${color.bold("version")}                  shows the currently installed version
+  ${color.bold("debug")}                    shows debug data
 
 OPTIONS
-  ${color.bold("--config")}              provide a custom configuration filename
-  ${color.bold("--online")}              check external links
+  ${color.bold("--config")}                 provide a custom configuration filename
+  ${color.bold("--online")}                 check external links
 `
 }

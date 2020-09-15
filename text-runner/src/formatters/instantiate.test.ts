7b2eebe20d27823696f91e05b4d03f4e136e3494
Kevin Goslar <kevin.goslar@gmail.com>
2020-09-15 09:12:19 -0500

    progress

--- text-runner/src/formatters/instantiate.test.ts
+++ text-runner/src/formatters/instantiate.test.ts
@@ -5,6 +5,7 @@ import { instantiateFormatter } from "./instantiate"
 import { SummaryFormatter } from "./implementations/summary-formatter"
 import { EventEmitter } from "events"
 import { defaultConfiguration } from "../configuration/default-configuration"
+import { ProgressFormatter } from "./implementations/progress-formatter"
 
 suite("instantiateFormatter()", function () {
   const emitter = new EventEmitter()
@@ -22,13 +23,12 @@ suite("instantiateFormatter()", function () {
     assert.instanceOf(formatter, DotFormatter)
   })
 
-  // NOTE: this creates an instance of the formatter right away.
-  //       refactor the formatter interface to have a "start" method where the progressbar is instantiated.
-  // TODO: this should work now
-  // test("request progress formatter", function () {
-  //   const formatter = instantiateFormatter("progress", 0, config)
-  //   assert.instanceOf(formatter, ProgressFormatter)
-  // })
+  test("request progress formatter", function () {
+    const config = defaultConfiguration()
+    config.formatterName = "progress"
+    const formatter = instantiateFormatter(config, emitter)
+    assert.instanceOf(formatter, ProgressFormatter)
+  })
 
   test("request summary formatter", function () {
     const config = defaultConfiguration()

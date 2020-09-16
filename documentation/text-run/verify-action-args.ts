95e8f0be4157cc654f9b433e6b8e7f02e448e874
Kevin Goslar <kevin.goslar@gmail.com>
2020-09-15 21:55:58 -0500

    docs

--- documentation/text-run/verify-action-args.ts
+++ documentation/text-run/verify-action-args.ts
@@ -1,5 +1,5 @@
 import * as assertNoDiff from "assert-no-diff"
-import { removeTrailingColon } from "../../text-runner/src/helpers/remove-trailing-colon"
+import { removeTrailingColon } from "../../text-runner-core/src/helpers/remove-trailing-colon"
 import { ActionArgs } from "text-runner-core"
 
 export default function verifyActionArgs(action: ActionArgs) {
--- documentation/text-run/verify-ast-node-attributes.ts
+++ documentation/text-run/verify-ast-node-attributes.ts
@@ -1,6 +1,6 @@
 import { AstNode } from "text-runner-core"
 import * as assertNoDiff from "assert-no-diff"
-import { removeTrailingColon } from "../../text-runner/src/helpers/remove-trailing-colon"
+import { removeTrailingColon } from "../../text-runner-core/src/helpers/remove-trailing-colon"
 import { ActionArgs } from "text-runner-core"
 
 export default async function verifyAstNodeAttributes(action: ActionArgs) {

5065ad3d22e24118e8168505027eaf4cced2d0d8
Kevin Goslar <kevin.goslar@gmail.com>
2020-09-16 12:47:54 -0500

    cleanup

--- shared/cucumber-steps/src/helpers/standardize-path.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.standardizePath = void 0;
-function standardizePath(filePath) {
-    return filePath.replace(/\\/g, "/");
-}
-exports.standardizePath = standardizePath;

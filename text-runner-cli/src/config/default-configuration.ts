203c88b63350906f52c59036110e6c7cd3b26a31
Kevin Goslar <kevin.goslar@gmail.com>
2020-09-17 16:11:08 -0500

    progress

---
+++ text-runner-cli/src/config/default-configuration.ts
@@ -0,0 +1,10 @@
+import { Publications } from "text-runner-core"
+import { UserProvidedConfiguration } from "./user-provided-configuration"
+
+/** default configuration values to use when no values are provided via CLI or config file */
+export function defaultConfiguration(): UserProvidedConfiguration {
+  return {
+    configFileName: "text-run.yml",
+    formatterName: "detailed",
+  }
+}
--- text-runner-cli/src/config/load-configuration.ts
+++ text-runner-cli/src/config/load-configuration.ts
@@ -4,7 +4,7 @@ import * as tr from "text-runner-core"
 import { determineConfigFilename } from "../config-file/determine-config-filename"
 import { loadConfigFile } from "../config-file/load-config-file"
 
-export async function loadConfiguration(cmdlineArgs: UserProvidedConfiguration): Promise<tr.Configuration> {
+export async function loadConfiguration(cmdlineArgs: UserProvidedConfiguration): Promise<UserProvidedConfiguration> {
   const configFilePath = await determineConfigFilename(cmdlineArgs)
   const configFileData = await loadConfigFile(configFilePath)
   return determineConfiguration(configFileData, cmdlineArgs)

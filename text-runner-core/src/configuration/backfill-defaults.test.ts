c8f9929b0ddf4d3e0043b3ce7af0043799919ac7
Kevin Goslar <kevin.goslar@gmail.com>
2020-09-18 16:19:36 -0500

    test

--- text-runner-core/src/configuration/backfill-defaults.test.ts
+++ text-runner-core/src/configuration/backfill-defaults.test.ts
@@ -6,4 +6,9 @@ suite("backfillDefaults", function () {
     const have = backfillDefaults({})
     assert.strictEqual(have.files, "**/*.md")
   })
+  test("input", function () {
+    const have = backfillDefaults({ files: "1.md", regionMarker: "foo" })
+    assert.strictEqual(have.files, "1.md")
+    assert.strictEqual(have.regionMarker, "foo")
+  })
 })

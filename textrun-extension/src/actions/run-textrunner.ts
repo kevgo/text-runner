2894b8d6fa6d311143c0e1937d4cf61da503c683
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-03 18:50:40 -0500

    progress

--- textrun-extension/src/actions/run-textrunner.ts
+++ textrun-extension/src/actions/run-textrunner.ts
@@ -1,21 +1,16 @@
-import { createObservableProcess } from "observable-process"
-import * as path from "path"
 import * as tr from "text-runner-core"
 
-import { callArgs } from "../helpers/call-args"
+import { ActivityCollector } from "../../../shared/cucumber-steps/src/activity-collector"
 
 /** runs Text-Runner in the workspace */
 export async function runTextrunner(action: tr.actions.Args): Promise<void> {
-  // TODO: call an internal Text-Runner API here, see https://github.com/kevgo/text-runner/issues/903
-  // TODO: call existing Text-Runner API here
   action.name("Running Text-Runner in workspace")
-  let textRunPath = path.join(action.configuration.sourceDir, "node_modules", ".bin", "text-run")
-  if (process.platform === "win32") textRunPath += ".cmd"
-  const trArgs = callArgs(textRunPath, process.platform)
-  const processor = createObservableProcess(trArgs, { cwd: action.configuration.workspace })
-  await processor.waitForEnd()
-  action.log(processor.output.fullText())
-  if (processor.exitCode && processor.exitCode !== 0) {
-    throw new Error(`the nested Text-Runner instance exited with code ${processor.exitCode}`)
-  }
+  const command = new tr.commands.Run({
+    sourceDir: action.configuration.workspace,
+    workspace: action.configuration.workspace,
+    emptyWorkspace: false,
+  })
+  const activityCollector = new ActivityCollector(command)
+  await command.execute()
+  action.log(activityCollector.results())
 }

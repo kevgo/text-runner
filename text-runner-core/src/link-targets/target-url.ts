d160475d63ba179d230fcde28feb866f5a8a5a8f
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-06 20:05:20 -0500

    Fully compatible Github anchor links (#1328)

--- text-runner-core/src/link-targets/target-url.ts
+++ text-runner-core/src/link-targets/target-url.ts
@@ -1,6 +1,8 @@
-import slugify = require("@sindresorhus/slugify")
+// @ts-expect-error: no types available
+import * as anchor from "anchor-markdown-header"
 
-const delRE = /\//g
 export function targetURL(targetName: string): string {
-  return slugify(targetName.toLowerCase().replace(delRE, ""))
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const link = anchor(targetName, "github.com") as string
+  return link.substring(link.indexOf("#") + 1, link.indexOf(")"))
 }

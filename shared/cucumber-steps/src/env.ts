b829fb97cb04adb9f75f8d7ef50cbc21ded9fad5
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-09 07:54:04 -0500

    Typecheck env.ts (#1347)

--- shared/cucumber-steps/src/env.ts
+++ shared/cucumber-steps/src/env.ts
@@ -5,55 +5,51 @@ import * as path from "path"
 import * as rimraf from "rimraf"
 import * as tmp from "tmp-promise"
 import * as util from "util"
+
+import { TRWorld } from "./world"
 const rimrafp = util.promisify(rimraf)
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 Before(async function () {
+  const world = this as TRWorld
   if (process.env.CUCUMBER_PARALLEL) {
     const tempDir = await tmp.dir()
-    this.rootDir = tempDir.path
+    world.rootDir = tempDir.path
   } else {
-    this.rootDir = path.join(process.cwd(), "tmp")
+    world.rootDir = path.join(process.cwd(), "tmp")
   }
   let rootDirExists = false
   try {
-    await fs.stat(this.rootDir)
+    await fs.stat(world.rootDir)
     rootDirExists = true
   } catch (e) {
     // nothing to do here
   }
   if (rootDirExists) {
-    await fs.rmdir(this.rootDir, { recursive: true })
+    await fs.rmdir(world.rootDir, { recursive: true })
   }
-  await fs.mkdir(this.rootDir, { recursive: true })
+  await fs.mkdir(world.rootDir, { recursive: true })
 })
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 After({ timeout: 20_000 }, async function (scenario) {
+  const world = this as TRWorld
   await endChildProcesses()
   if (scenario.result.status === "failed") {
-    console.log("\ntest artifacts are located in", this.rootDir)
+    console.log("\ntest artifacts are located in", world.rootDir)
   } else {
     // NOTE: need rimraf here because Windows requires to retry this for a few times
     // TODO: replace with fs
-    await rimrafp(this.rootDir)
+    await rimrafp(world.rootDir)
   }
 })
 
-Before({ tags: "@verbose" }, function () {
-  this.verbose = true
-})
-
-After({ tags: "@verbose" }, function () {
-  this.verbose = false
-})
-
 Before({ tags: "@debug" }, function () {
-  this.debug = true
-  this.verbose = true
+  const world = this as TRWorld
+  world.debug = true
 })
 
 After({ tags: "@debug" }, function () {
-  this.debug = false
-  this.verbose = false
+  const world = this as TRWorld
+  world.debug = false
 })

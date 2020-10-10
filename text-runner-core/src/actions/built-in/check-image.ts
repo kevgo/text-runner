a369fb54cbb24dcdadfa9874f9e84fb505201757
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-10 16:46:40 -0500

    Convert checkImage.checkLocalImage ot FullFile

--- text-runner-core/src/actions/built-in/check-image.ts
+++ text-runner-core/src/actions/built-in/check-image.ts
@@ -1,14 +1,14 @@
 import { promises as fs } from "fs"
 import got from "got"
-import * as path from "path"
 
 import * as configuration from "../../configuration/index"
+import * as files from "../../filesystem"
 import * as actions from "../index"
 
 /** The "checkImage" action checks for broken images. */
 export async function checkImage(action: actions.Args): Promise<number | void> {
   const node = action.region[0]
-  let imagePath = node.attributes.src
+  const imagePath = node.attributes.src
   if (!imagePath) {
     throw new Error("image tag without source")
   }
@@ -18,19 +18,22 @@ export async function checkImage(action: actions.Args): Promise<number | void> {
     return result
   } else {
     // local image here
-    if (!imagePath.startsWith("/")) {
-      imagePath = path.join(path.dirname(node.location.file.platformified()), imagePath)
+    let imageFullFile: files.FullFile
+    if (imagePath.startsWith("/")) {
+      imageFullFile = new files.FullFile(imagePath)
+    } else {
+      imageFullFile = new files.FullFile(node.location.file.directory().joinStr(imagePath))
     }
-    const result = await checkLocalImage(imagePath, action.configuration)
+    const result = await checkLocalImage(imageFullFile, action.configuration)
     return result
   }
 }
 
-async function checkLocalImage(imagePath: string, c: configuration.Data): Promise<void> {
+async function checkLocalImage(image: files.FullFile, c: configuration.Data): Promise<void> {
   try {
-    await fs.stat(c.sourceDir.joinStr(imagePath))
+    await fs.stat(c.sourceDir.joinFullFile(image).platformified())
   } catch (err) {
-    throw new Error(`image ${imagePath} does not exist`)
+    throw new Error(`image ${image} does not exist`)
   }
 }
 

c1ae313823131f66afa5daa9d6ea899b972b0208
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-10 10:57:20 -0500

    progress

--- text-runner-core/src/filesystem/location.ts
+++ text-runner-core/src/filesystem/location.ts
@@ -12,18 +12,22 @@ export class Location {
     this.line = line
   }
 
+  /** provides an empty location for testing */
   static scaffold(): Location {
     return new Location(new files.SourceDir(""), new files.FullFile(""), 0)
   }
 
+  /** provides the absolute path to the file in this location */
   absoluteFile(): files.AbsoluteFile {
     return this.sourceDir.joinFullFile(this.file)
   }
 
+  /** provides a copy of this Location with the given file */
   withFile(file: files.FullFile): Location {
     return new Location(this.sourceDir, file, this.line)
   }
 
+  /** provides a copy of this Location with the given line */
   withLine(line: number): Location {
     return new Location(this.sourceDir, this.file, line)
   }

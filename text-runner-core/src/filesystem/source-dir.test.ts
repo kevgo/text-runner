846bc62ed1ff98b5774fed7802e7c2b7c0c65b5d
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-10 07:49:42 -0500

    tests

--- text-runner-core/src/filesystem/source-dir.test.ts
+++ text-runner-core/src/filesystem/source-dir.test.ts
@@ -10,4 +10,56 @@ suite("sourceDir", function () {
     const want = new files.AbsoluteDir("/home/acme/text-runner/src")
     assert.deepEqual(have, want)
   })
+
+  test("joinFullFile", function () {
+    const sourceDir = new files.SourceDir("/home/acme/text-runner")
+    const fullFile = new files.FullFile("src/README.md")
+    const have = sourceDir.joinFullFile(fullFile)
+    const want = new files.AbsoluteFile("/home/acme/text-runner/src/README.md")
+    assert.deepEqual(have, want)
+  })
+
+  test("joinFullStr", function () {
+    const sourceDir = new files.SourceDir("/home/acme/text-runner")
+    const have = sourceDir.joinStr("src/README.md")
+    assert.deepEqual(have, "/home/acme/text-runner/src/README.md")
+  })
+
+  suite("platformified", function () {
+    test("on *nix", function () {
+      if (process.platform === "win32") {
+        return
+      }
+      const sourceDir = new files.SourceDir("/home/acme/textrun")
+      const have = sourceDir.platformified()
+      assert.equal(have, "/home/acme/textrun")
+    })
+    test("on Windows", function () {
+      if (process.platform !== "win32") {
+        return
+      }
+      const sourceDir = new files.SourceDir("c:\\Users\\acme\\textrun")
+      const have = sourceDir.platformified()
+      assert.equal(have, "c:\\Users\\acme\\textrun")
+    })
+  })
+
+  suite("unixified", function () {
+    test("on *nix", function () {
+      if (process.platform === "win32") {
+        return
+      }
+      const sourceDir = new files.SourceDir("/home/acme/textrun")
+      const have = sourceDir.platformified()
+      assert.equal(have, "/home/acme/textrun")
+    })
+    test("on Windows", function () {
+      if (process.platform !== "win32") {
+        return
+      }
+      const sourceDir = new files.SourceDir("c:\\Users\\acme\\textrun")
+      const have = sourceDir.platformified()
+      assert.equal(have, "c:/Users/acme/textrun")
+    })
+  })
 })

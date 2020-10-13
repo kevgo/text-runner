96e22fac07853cdb30b55d40f18f02a61f4a72d6
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-13 17:12:27 -0500

    Installed executable

---
+++ textrun-npm/src/actions/installed-executable.ts
@@ -0,0 +1,17 @@
+import * as color from "colorette"
+import * as tr from "text-runner-core"
+
+import { trimDollar } from "../helpers/trim-dollar"
+
+export function executable(action: tr.actions.Args): void {
+  const commandName = trimDollar(action.region.text().trim())
+  if (commandName === "") {
+    throw new Error("No npm package installation command found")
+  }
+  action.name(`npm package exports executable ${color.cyan(commandName)}`)
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const pkgData = require(action.configuration.sourceDir.joinStr("package.json"))
+  if (!Object.keys(pkgData.bin).includes(commandName)) {
+    throw new Error(`package.json does not export a "${commandName}" command`)
+  }
+}

7cd3378690d36799b02bc5824bf0a0ce47aeee2b
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-13 18:30:45 -0500

    npm/installed-executable

--- textrun-npm/src/actions/installed-executable.ts
+++ textrun-npm/src/actions/installed-executable.ts
@@ -1,17 +1,28 @@
 import * as color from "colorette"
+import { promises as fsp } from "fs"
+import * as path from "path"
 import * as tr from "text-runner-core"
 
 import { trimDollar } from "../helpers/trim-dollar"
 
-export function executable(action: tr.actions.Args): void {
+export async function installedExecutable(action: tr.actions.Args): Promise<void> {
   const commandName = trimDollar(action.region.text().trim())
   if (commandName === "") {
-    throw new Error("No npm package installation command found")
+    throw new tr.UserError(
+      "Executable name not specified",
+      "This action checks for executables provided by npm modules that you have installed. They are typically inside node_modules/.bin. Please provide the name of the executable.",
+      action.location
+    )
   }
-  action.name(`npm package exports executable ${color.cyan(commandName)}`)
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const pkgData = require(action.configuration.sourceDir.joinStr("package.json"))
-  if (!Object.keys(pkgData.bin).includes(commandName)) {
-    throw new Error(`package.json does not export a "${commandName}" command`)
+  action.name(`installed npm executable ${color.cyan(commandName)}`)
+  const fullPath = path.join("node_modules", ".bin", commandName)
+  try {
+    await fsp.stat(action.configuration.sourceDir.joinStr(fullPath))
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      throw new Error(`no installed executable ${color.cyan(fullPath)} found`)
+    } else {
+      throw e
+    }
   }
 }
--- textrun-npm/src/index.ts
+++ textrun-npm/src/index.ts
@@ -1,7 +1,9 @@
 import { exportedExecutable } from "./actions/exported-executable"
 import { install } from "./actions/install"
+import { installedExecutable } from "./actions/installed-executable"
 
 export const textrunActions = {
   exportedExecutable,
   install,
+  installedExecutable,
 }

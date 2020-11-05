4a8496da376e90c9732b1ef9804875e3c42e580c
Kevin Goslar <kevin.goslar@gmail.com>
2020-11-05 16:42:19 -0600

    progress

--- text-runner-cli/src/api.test.ts
+++ text-runner-cli/src/api.test.ts
@@ -1,9 +1,9 @@
 import { assert } from "chai"
 
 suite("JS API export", function () {
-  test("exports", function () {
+  test("exports", async function () {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const cli = require("..")
+    const cli = await import("..")
     assert.exists(cli.commands)
   })
 })

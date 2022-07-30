bddf75155ff441db667f63c7b4d8d9a63f49647c
Kevin Goslar <kevin.goslar@gmail.com>
2022-07-30 15:25:21 -0500

    error.test.ts

---
+++ text-runner-core/src/errors/error.test.ts
@@ -0,0 +1,27 @@
+import { assert } from "chai"
+
+import { errorMessage } from "./error"
+import { UserError } from "./user-error"
+
+suite("errorMessage", function () {
+  test("Error", function () {
+    const give = new Error("hello")
+    const want = "hello"
+    const have = errorMessage(give)
+    assert.equal(have, want)
+  })
+
+  test("number", function () {
+    const give = 123
+    const want = "123"
+    const have = errorMessage(give)
+    assert.equal(have, want)
+  })
+
+  test("UserError", function () {
+    const give = new UserError("hello", "world")
+    const want = "hello"
+    const have = errorMessage(give)
+    assert.equal(have, want)
+  })
+})

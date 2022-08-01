e83ac390583627a2938219c746436d448d12d201
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-01 10:50:49 -0500

    name-full.ts name-short.ts

--- textrun-action/src/name-full.ts
+++ textrun-action/src/name-full.ts
@@ -1,3 +1,4 @@
+import { promises as fs } from "fs"
 import * as tr from "text-runner-core"
 
 import { IndexFile, PackageJson } from "./files"
@@ -6,8 +7,10 @@ export async function nameFull(action: tr.actions.Args): Promise<void> {
   const want = action.region.text()
   action.name(`verify full name of action "${want}"`)
   const wantStd = tr.actions.name(want)
-  const pkgJson: PackageJson = await import(action.configuration.sourceDir.joinStr("package.json"))
-  const main: IndexFile = await import(action.configuration.sourceDir.joinStr(pkgJson.main))
+  const pkgJsonPath = action.configuration.sourceDir.joinStr("package.json")
+  const pkgJson: PackageJson = JSON.parse(await fs.readFile(pkgJsonPath, "utf-8"))
+  const mainPath = action.configuration.sourceDir.joinStr(pkgJson.main)
+  const main: IndexFile = await import(mainPath)
   const allNames = Object.keys(main.textrunActions)
   const allNamesStd = allNames.map(tr.actions.name)
   if (!allNamesStd.includes(wantStd)) {

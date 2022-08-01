97b54555f7fe8c4fa2cb591cad1780d2b7d073e6
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-01 10:45:33 -0500

    Type-check package.json in textrun-action

--- textrun-action/src/name-full.ts
+++ textrun-action/src/name-full.ts
@@ -1,10 +1,12 @@
 import * as tr from "text-runner-core"
 
+import { PackageJson } from "./files"
+
 export async function nameFull(action: tr.actions.Args): Promise<void> {
   const want = action.region.text()
   action.name(`verify full name of action "${want}"`)
   const wantStd = tr.actions.name(want)
-  const pkgJson = await import(action.configuration.sourceDir.joinStr("package.json"))
+  const pkgJson: PackageJson = await import(action.configuration.sourceDir.joinStr("package.json"))
   const main = await import(action.configuration.sourceDir.joinStr(pkgJson.main))
   const allNames = Object.keys(main.textrunActions)
   const allNamesStd = allNames.map(tr.actions.name)
--- textrun-action/src/name-short.ts
+++ textrun-action/src/name-short.ts
@@ -1,10 +1,14 @@
 import { promises as fs } from "fs"
 import * as tr from "text-runner-core"
 
+import { PackageJson } from "./files"
+
 export async function nameShort(action: tr.actions.Args): Promise<void> {
   const want = action.region.text()
   const wantStd = tr.actions.name(want)
-  const pkgJson = JSON.parse(await fs.readFile(action.configuration.sourceDir.joinStr("package.json"), "utf-8"))
+  const pkgJson: PackageJson = JSON.parse(
+    await fs.readFile(action.configuration.sourceDir.joinStr("package.json"), "utf-8")
+  )
   const main = await import(action.configuration.sourceDir.joinStr(pkgJson.main))
   const allNames = Object.keys(main.textrunActions)
   const allNamesStd = allNames.map(tr.actions.name)

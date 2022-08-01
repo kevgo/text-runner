6785a4bcb4858fa70f46812260728e49214b5322
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-01 11:59:51 -0500

    Update eslint (#1440)

--- textrun-npm/src/actions/install.ts
+++ textrun-npm/src/actions/install.ts
@@ -11,10 +11,8 @@ export async function install(action: tr.actions.Args): Promise<void> {
     throw new Error("no installation instructions found")
   }
   action.name(`check npm package name in ${color.cyan(installText)}`)
-
   const dir = action.region[0]?.attributes?.dir || ""
   const pkg: PackageJson = await fs.readJSON(action.configuration.sourceDir.joinStr(dir, "package.json"))
-
   if (missesPackageName(installText, pkg.name)) {
     throw new Error(
       `installation instructions ${color.cyan(installText)} don't contain expected npm package name ${color.cyan(

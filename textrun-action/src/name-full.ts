91fe584363113f5469b0a2201836525bc96f7721
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-01 10:47:04 -0500

    name-full.ts name-short.ts

--- textrun-action/src/name-full.ts
+++ textrun-action/src/name-full.ts
@@ -1,13 +1,13 @@
 import * as tr from "text-runner-core"
 
-import { PackageJson } from "./files"
+import { IndexFile, PackageJson } from "./files"
 
 export async function nameFull(action: tr.actions.Args): Promise<void> {
   const want = action.region.text()
   action.name(`verify full name of action "${want}"`)
   const wantStd = tr.actions.name(want)
   const pkgJson: PackageJson = await import(action.configuration.sourceDir.joinStr("package.json"))
-  const main = await import(action.configuration.sourceDir.joinStr(pkgJson.main))
+  const main: IndexFile = await import(action.configuration.sourceDir.joinStr(pkgJson.main))
   const allNames = Object.keys(main.textrunActions)
   const allNamesStd = allNames.map(tr.actions.name)
   if (!allNamesStd.includes(wantStd)) {
--- textrun-action/src/name-short.ts
+++ textrun-action/src/name-short.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from "fs"
 import * as tr from "text-runner-core"
 
-import { PackageJson } from "./files"
+import { IndexFile, PackageJson } from "./files"
 
 export async function nameShort(action: tr.actions.Args): Promise<void> {
   const want = action.region.text()
@@ -9,7 +9,7 @@ export async function nameShort(action: tr.actions.Args): Promise<void> {
   const pkgJson: PackageJson = JSON.parse(
     await fs.readFile(action.configuration.sourceDir.joinStr("package.json"), "utf-8")
   )
-  const main = await import(action.configuration.sourceDir.joinStr(pkgJson.main))
+  const main: IndexFile = await import(action.configuration.sourceDir.joinStr(pkgJson.main))
   const allNames = Object.keys(main.textrunActions)
   const allNamesStd = allNames.map(tr.actions.name)
   if (!allNamesStd.includes(wantStd)) {

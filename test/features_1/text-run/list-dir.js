c8f1d58e1f21d70d146f7a0ad1091c5f0e223194
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-18 14:45:41 -0500

    1 2 3 text-run.yml 1 list-dir.js 1 2 3 text-run.yml 1 2 3 text-run.yml 1 2 3 text-run.yml 1 2 text-run.yml 1 2 3 text-run.yml 1 text-run.yml list-dir.js

--- test/features_1/text-run/list-dir.js
+++
@@ -1,5 +0,0 @@
-import {promises as fs} from "fs"
-export default async (action) => {
-  const items = await fs.readdir(action.configuration.workspace.platformified())
-  action.name(`${items.length} workspace files (${items.join(", ")})`)
-}

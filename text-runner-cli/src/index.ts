4224bf2c5264ca3e187a3cc436bcbc4dc63edb75
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-18 11:26:47 -0500

    Use top-level await

--- text-runner-cli/src/index.ts
+++ text-runner-cli/src/index.ts
@@ -9,36 +9,31 @@ import * as formatters from "./formatters/index.js"
 
 cliCursor.hide()
 
-async function main() {
-  let errorCount = 0
-  try {
-    const { commandName, cmdLineConfig, debugSubcommand } = cmdLine.parse(process.argv)
-    const fileConfig = await configFile.load(cmdLineConfig)
-    const userConfig = fileConfig.merge(cmdLineConfig)
-    const command = commands.instantiate(commandName, userConfig, debugSubcommand)
-    const formatter = formatters.instantiate(userConfig.formatterName || "detailed", command)
-    const activityCollector = new tr.ActivityCollector(command)
-    await command.execute()
-    const results = activityCollector.results()
-    if (["dynamic", "run", "static"].includes(commandName)) {
-      formatter.finish({ results })
-    }
-    errorCount = results.errorCount()
-  } catch (e) {
-    errorCount += 1
-    if (e instanceof tr.UserError) {
-      formatters.printUserError(e)
-    } else if (e instanceof Error) {
-      console.log(e.message)
-      console.log(e.stack)
-    } else {
-      console.log(e)
-    }
-  } finally {
-    await endChildProcesses()
+let errorCount = 0
+try {
+  const { commandName, cmdLineConfig, debugSubcommand } = cmdLine.parse(process.argv)
+  const fileConfig = await configFile.load(cmdLineConfig)
+  const userConfig = fileConfig.merge(cmdLineConfig)
+  const command = commands.instantiate(commandName, userConfig, debugSubcommand)
+  const formatter = formatters.instantiate(userConfig.formatterName || "detailed", command)
+  const activityCollector = new tr.ActivityCollector(command)
+  await command.execute()
+  const results = activityCollector.results()
+  if (["dynamic", "run", "static"].includes(commandName)) {
+    formatter.finish({ results })
   }
-  process.exit(errorCount)
+  errorCount = results.errorCount()
+} catch (e) {
+  errorCount += 1
+  if (e instanceof tr.UserError) {
+    formatters.printUserError(e)
+  } else if (e instanceof Error) {
+    console.log(e.message)
+    console.log(e.stack)
+  } else {
+    console.log(e)
+  }
+} finally {
+  await endChildProcesses()
 }
-main().catch(function (e) {
-  throw e
-})
+process.exit(errorCount)

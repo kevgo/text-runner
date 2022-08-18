88001260d0005b1c5bfbe21df54bb5d81364d98e
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-18 11:15:29 -0500

    api.test.ts

--- text-runner-cli/src/api.test.ts
+++ text-runner-cli/src/api.test.ts
@@ -3,7 +3,6 @@ import * as textRunner from "text-runner"
 
 suite("JS API export", function () {
   test("exports", function () {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
     assert.exists(textRunner.commands)
   })
 })

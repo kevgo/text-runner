0cfd1949559a8258109132fdef35331c524ed81b
Kevin Goslar <kevin.goslar@gmail.com>
2022-08-18 16:35:36 -0500

    bin imports index

--- text-runner-cli/bin/text-run.mjs
+++ text-runner-cli/bin/text-run.mjs
@@ -1,13 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env node
 
-if [ -L "$0" ]; then
-  # the binary is a symlink
-  SYMLINK_TARGET=$(readlink -f "$0")
-  BIN_DIR=$(dirname -- "$SYMLINK_TARGET")
-  CLI_DIR=$(dirname -- "$BIN_DIR")
-else
-  # the binary is not a symlink, i.e. we are in the node_modules folder
-  CLI_DIR=node_modules/text-runner
-fi
+import {main} from "../dist/index.js"
 
-node --experimental-loader ts-node/esm --no-warnings "$CLI_DIR/dist/index.js" "$@"
+main().catch(function(e) {
+  throw e
+})
--- text-runner-cli/src/index.ts
+++ text-runner-cli/src/index.ts
@@ -7,33 +7,35 @@ import * as commands from "./commands/index.js"
 import * as configFile from "./config-file.js"
 import * as formatters from "./formatters/index.js"
 
-cliCursor.hide()
+export async function main() {
+  cliCursor.hide()
 
-let errorCount = 0
-try {
-  const { commandName, cmdLineConfig, debugSubcommand } = cmdLine.parse(process.argv)
-  const fileConfig = await configFile.load(cmdLineConfig)
-  const userConfig = fileConfig.merge(cmdLineConfig)
-  const command = commands.instantiate(commandName, userConfig, debugSubcommand)
-  const formatter = formatters.instantiate(userConfig.formatterName || "detailed", command)
-  const activityCollector = new tr.ActivityCollector(command)
-  await command.execute()
-  const results = activityCollector.results()
-  if (["dynamic", "run", "static"].includes(commandName)) {
-    formatter.finish({ results })
+  let errorCount = 0
+  try {
+    const { commandName, cmdLineConfig, debugSubcommand } = cmdLine.parse(process.argv)
+    const fileConfig = await configFile.load(cmdLineConfig)
+    const userConfig = fileConfig.merge(cmdLineConfig)
+    const command = commands.instantiate(commandName, userConfig, debugSubcommand)
+    const formatter = formatters.instantiate(userConfig.formatterName || "detailed", command)
+    const activityCollector = new tr.ActivityCollector(command)
+    await command.execute()
+    const results = activityCollector.results()
+    if (["dynamic", "run", "static"].includes(commandName)) {
+      formatter.finish({ results })
+    }
+    errorCount = results.errorCount()
+  } catch (e) {
+    errorCount += 1
+    if (e instanceof tr.UserError) {
+      formatters.printUserError(e)
+    } else if (e instanceof Error) {
+      console.log(e.message)
+      console.log(e.stack)
+    } else {
+      console.log(e)
+    }
+  } finally {
+    await endChildProcesses()
   }
-  errorCount = results.errorCount()
-} catch (e) {
-  errorCount += 1
-  if (e instanceof tr.UserError) {
-    formatters.printUserError(e)
-  } else if (e instanceof Error) {
-    console.log(e.message)
-    console.log(e.stack)
-  } else {
-    console.log(e)
-  }
-} finally {
-  await endChildProcesses()
+  process.exit(errorCount)
 }
-process.exit(errorCount)

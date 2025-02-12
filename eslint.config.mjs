a4859efc9a82b0abb3b30788fa3ee4db8ad2d6d9
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-12 17:17:31 -0600

    eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 
+
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic
 );

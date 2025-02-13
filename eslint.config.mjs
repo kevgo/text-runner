8eb7592c0be69d91f76010774b3864b5b634a3fc
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-13 05:47:43 -0600

    .eslintrc_old.json eslint.config.mjs package.json yarn.lock

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
 
 import eslintPlugin from "@typescript-eslint/eslint-plugin"
-import eslintParser from "@typescript-eslint/parser"
+import tsParser from "@typescript-eslint/parser"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
 
 export default [
   {
     files: ["**/*.ts"],
-    ignores: ["shared/cucumber-steps/dist/when-steps.js"],
+    ignores: ["**/node_modules/", ".git/", "**/dist/**", "shared/cucumber-steps/dist/"],
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: "module",
-      parser: eslintParser,
+      parser: tsParser,
       parserOptions: {
         project: "./tsconfig.json",
       },
@@ -20,21 +21,30 @@ export default [
         process: "readonly",
       },
     },
-    plugins: { "@typescript-eslint": eslintPlugin },
+    plugins: { "@typescript-eslint": eslintPlugin, "simple-import-sort": simpleImportSort },
     rules: {
       ...eslintPlugin.configs.recommended.rules,
+      "no-var": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-implied-eval": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
     },
   },
-  {
-    files: ["examples/custom-action-commonjs/text-run/hello-world.js"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "commonjs",
-      globals: {
-        console: "readonly",
-        module: "readonly",
-        process: "readonly",
-      },
-    },
-  },
+  // {
+  //   files: ["examples/custom-action-commonjs/text-run/hello-world.js"],
+  //   languageOptions: {
+  //     ecmaVersion: 2021,
+  //     sourceType: "commonjs",
+  //     globals: {
+  //       console: "readonly",
+  //       module: "readonly",
+  //       process: "readonly",
+  //     },
+  //   },
+  // },
 ]

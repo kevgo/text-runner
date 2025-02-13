b48c2b2b967b0ee5854b1ef87a01900c62a3ccc1
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-13 06:42:22 -0600

    Remove deprecated Cucumber option

--- textrun-extension/cucumber.cjs
+++ textrun-extension/cucumber.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-  default: "--import '../shared/cucumber-steps/dist/*.js' --fail-fast --publish-quiet",
+  default: "--import '../shared/cucumber-steps/dist/*.js' --fail-fast",
 }

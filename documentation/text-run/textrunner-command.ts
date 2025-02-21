39155706933f82858735326dbcb67669ce04840b
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-21 10:02:45 -0600

    textrunner-command.ts

--- documentation/text-run/textrunner-command.ts
+++ documentation/text-run/textrunner-command.ts
@@ -1,7 +1,7 @@
 import * as textRunner from "text-runner"
 
 export default function textrunnerCommand(action: textRunner.actions.Args): void {
-  const documented = action.region.text().replace("text-runner ", "")
+  const documented = action.region.text().replace("text-run ", "")
   action.name(`Text-Runner command: ${documented}`)
   const existing = Object.keys(textRunner.commands).map(s => s.toLowerCase())
   if (!existing.includes(documented)) {

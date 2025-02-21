ce379ab43b4b6a4f30c6122927fd1728dd79f496
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-21 10:27:45 -0600

    Fix linter warnings (#1501)

--- text-runner-cli/src/start.ts
+++ text-runner-cli/src/start.ts
@@ -1,8 +1,9 @@
 import cliCursor from "cli-cursor"
+
 import { main } from "./main.js"
 
 export async function start() {
   cliCursor.hide()
-  let errorCount = await main(process.argv)
+  const errorCount = await main(process.argv)
   process.exit(errorCount)
 }

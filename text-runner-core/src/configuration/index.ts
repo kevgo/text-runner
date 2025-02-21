26d9b025a683f698092745207f38e0bf38899a36
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-21 14:42:08 -0600

    More precise type export expression

--- text-runner-core/src/configuration/index.ts
+++ text-runner-core/src/configuration/index.ts
@@ -1,4 +1,4 @@
-export { APIData, CompleteAPIData, Data } from "./data.js"
+export type { APIData, CompleteAPIData, Data } from "./data.js"
 export { addDefaults, defaults } from "./defaults.js"
 export { Publication } from "./publication.js"
 export { Publications } from "./publications.js"

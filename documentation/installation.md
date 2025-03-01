f79d554a7ddf431f68d533f3c86040c89694ea31
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-01 06:53:56 -0600

    Create JSON-schema in scaffolded configuration (#1543)

--- documentation/installation.md
+++ documentation/installation.md
@@ -53,6 +53,9 @@ This creates a configuration file called <a type="workspace/existing-file">
 
 ```jsonc
 {
+  // link to the JSON schema that defines this document
+  "$schema": "https://raw.githubusercontent.com/kevgo/text-runner/refs/heads/main/documentation/text-runner.schema.json",
+
   // white-list for files to test
   // This is a glob expression, see https://github.com/isaacs/node-glob#glob-primer
   // The folder "node_modules" is already excluded.
--- text-runner-cli/src/config-file.ts
+++ text-runner-cli/src/config-file.ts
@@ -14,6 +14,9 @@ export async function create(cmdLineArgs: config.Data): Promise<void> {
   await fs.writeFile(
     cmdLineArgs.configFileName || "text-runner.jsonc",
     `{
+  // link to the JSON schema that defines this document
+  "$schema": "https://raw.githubusercontent.com/kevgo/text-runner/refs/heads/main/documentation/text-runner.schema.json",
+
   // white-list for files to test
   // This is a glob expression, see https://github.com/isaacs/node-glob#glob-primer
   // The folder "node_modules" is already excluded.

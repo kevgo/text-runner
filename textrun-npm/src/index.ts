6048cc671f0858cfad15c011f1531b17548f005a
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-03 17:44:09 -0600

    script.ts index.ts

--- textrun-npm/src/index.ts
+++ textrun-npm/src/index.ts
@@ -3,11 +3,11 @@ import * as textRunner from "text-runner"
 import { exportedExecutable } from "./actions/exported-executable.js"
 import { install } from "./actions/install.js"
 import { installedExecutable } from "./actions/installed-executable.js"
-import { scriptName } from "./actions/script-name.js"
+import { script } from "./actions/script.js"
 
 export const textrunActions: textRunner.exports.TextrunActions = {
   exportedExecutable,
   install,
   installedExecutable,
-  scriptName
+  script
 }

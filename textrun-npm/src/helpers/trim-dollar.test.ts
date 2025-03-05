3233ee65aa495aea46c37129f515d1244816d689
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-05 06:23:49 -0600

    .prettierignore .prettierrc dprint.json package.json .prettierignore dprint.json package.json install.ts trim-dollar.test.ts tsconfig-build.json yarn.lock

--- textrun-npm/src/helpers/trim-dollar.test.ts
+++ textrun-npm/src/helpers/trim-dollar.test.ts
@@ -3,14 +3,14 @@ import { suite, test } from "node:test"
 
 import { trimDollar } from "./trim-dollar.js"
 
-suite("trimDollar", function () {
+suite("trimDollar", function() {
   const tests = {
     "$ foo": "foo",
     "$   foo": "foo",
     foo: "foo"
   }
   for (const [give, want] of Object.entries(tests)) {
-    test(`${give} --> ${want}`, function () {
+    test(`${give} --> ${want}`, function() {
       assert.equal(want, trimDollar(give))
     })
   }

4d043c86394c952f71e6b1ed3e518e746e203ed0
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-06 16:06:24 -0600

    textrun-shell.js

--- documentation/textrun-shell.js
+++ documentation/textrun-shell.js
@@ -1,10 +1,10 @@
-import path from "path"
+import * as path from "path"
 import * as url from "url"
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
 
 export default {
   globals: {
-    "text-runner": path.join(__dirname, "..", "text-runner-cli", "bin", "text-runner")
+    "text-runner": path.join(__dirname, "node_modules", ".bin", "text-runner")
   }
 }

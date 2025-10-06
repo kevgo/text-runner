073f8e7e9308e298ec44afbdf85e857e0cc3c1dc
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-06 11:58:05 -0400

    Improve E2E tests for workspace (#1632)

--- textrun-workspace/src/actions/additional-file-content.ts
+++ textrun-workspace/src/actions/additional-file-content.ts
@@ -5,8 +5,24 @@ import * as textRunner from "text-runner-engine"
 
 export async function additionalFileContent(action: textRunner.actions.Args): Promise<void> {
   const fileNameAttribute = action.region[0].attributes["filename"]
+  if (fileNameAttribute === "") {
+    throw new textRunner.UserError(`attribute "filename" is empty`, "")
+  }
   const fileName = fileNameAttribute || action.region.textInNodeOfType("em", "strong")
   const dirAttribute = action.region[0].attributes["dir"]
+  if (dirAttribute === "") {
+    throw new textRunner.UserError(`attribute "dir" is empty`, "")
+  }
+  if (dirAttribute !== undefined) {
+    const dirPath = action.configuration.workspace.joinStr(dirAttribute)
+    try {
+      await fs.access(dirPath, fs.constants.F_OK)
+    } catch (e) {
+      if (e instanceof Error) {
+        throw new textRunner.UserError(`dir "${dirAttribute}" doesn't exist`, e.message)
+      }
+    }
+  }
   const fileRelPath = dirAttribute ? path.join(dirAttribute, fileName) : fileName
   action.name(`append to file ${color.cyan(fileRelPath)}`)
   const fullPath = action.configuration.workspace.joinStr(fileRelPath)
@@ -14,7 +30,7 @@ export async function additionalFileContent(action: textRunner.actions.Args): Pr
     await fs.access(fullPath, fs.constants.F_OK)
   } catch (e) {
     if (e instanceof Error) {
-      throw new textRunner.UserError(`file ${fileRelPath} doesn't exist`, e.message)
+      throw new textRunner.UserError(`file "${fileRelPath}" doesn't exist`, e.message)
     }
   }
   const content = fileNameAttribute ? action.region.text() : action.region.textInNodeOfType("fence", "code")

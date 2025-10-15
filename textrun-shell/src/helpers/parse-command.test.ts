c298a663023acc0209c024cd9bdd5dfac11a7751
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-15 12:46:47 -0500

    parse-command.test.ts

--- textrun-shell/src/helpers/parse-command.test.ts
+++ textrun-shell/src/helpers/parse-command.test.ts
@@ -17,12 +17,22 @@ suite("parseCommand", () => {
   })
 
   test("multiple commands", () => {
-    const have = parseCommand("echo one\necho two", noGlobalize)
-    const want = "echo one && echo two"
+    const have = parseCommand("echo one\necho two\necho three", noGlobalize)
+    const want = "echo one && echo two && echo three"
+    assert.equal(have, want)
+  })
+
+  test("one", () => {
+    const have = parseCommand("myapp foo", fakeGlobalize)
+    const want = "/globalized/path/myapp foo"
     assert.equal(have, want)
   })
 })
 
 function noGlobalize(x: string): string {
   return x
+}
+
+function fakeGlobalize(x: string): string {
+  return `/globalized/path/${x}`
 }

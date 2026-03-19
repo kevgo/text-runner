ad70fdda2066c762e444e0485a642e04a8831101
Kevin Goslar <kevin.goslar@gmail.com>
2026-03-18 20:10:46 -0500

    package.json hello-world.ts

--- examples/custom-action-typescript/text-runner/hello-world.ts
+++ examples/custom-action-typescript/text-runner/hello-world.ts
@@ -1,4 +1,4 @@
-import * as textRunner from "text-runner"
+import type * as textRunner from "text-runner"
 
 export default function HelloWorld(action: textRunner.actions.Args): void {
   action.log("Hello World from TypeScript!")

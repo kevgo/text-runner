e3f0b80be60fec05f4fdb7fa948ecb3c6000bb51
Kevin Goslar <kevin.goslar@gmail.com>
2026-03-21 15:25:40 -0500

    index.ts

--- text-runner-engine/src/events/index.ts
+++ text-runner-engine/src/events/index.ts
@@ -12,6 +12,7 @@ export interface Failed {
   readonly status: "failed"
 }
 
+// biome-ignore lint/suspicious/noExplicitAny: too many possible types here
 export type Handler = (arg: any) => void
 
 /** defines the events that a command can emit. */

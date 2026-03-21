8a93c453c56c5829db61ddc06259712497af91b8
Kevin Goslar <kevin.goslar@gmail.com>
2026-03-21 09:18:47 -0500

    actions.ts

--- text-runner-engine/src/actions/actions.ts
+++ text-runner-engine/src/actions/actions.ts
@@ -18,6 +18,7 @@ export class Actions {
     return Object.keys(this.list).sort()
   }
 
+  // biome-ignore lint/suspicious/noExplicitAny: action is some sort of function
   register(fileBasedName: string, action: any): void {
     if (typeof action === "function") {
       this.list[fileBasedName] = action

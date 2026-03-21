ee46ec53a09236488b9404f632cbaad615249ef3
Kevin Goslar <kevin.goslar@gmail.com>
2026-03-21 16:41:57 -0500

    Replace ESLint with Biome for textrun-shell (#1676)

--- textrun-shell/src/actions/server.ts
+++ textrun-shell/src/actions/server.ts
@@ -1,6 +1,6 @@
 import { styleText } from "node:util"
 import * as observableProcess from "observable-process"
-import * as textRunner from "text-runner-engine"
+import type * as textRunner from "text-runner-engine"
 import * as trExt from "textrun-extension"
 
 import { Configuration } from "../helpers/configuration.js"
@@ -14,7 +14,7 @@ import { parseCommand } from "../helpers/parse-command.js"
 export async function server(action: textRunner.actions.Args): Promise<void> {
   const configPath = action.configuration.sourceDir.joinStr("textrun-shell.js")
   const config = await Configuration.load(configPath)
-  var commandText = action.region[0].attributes["command"]
+  var commandText = action.region[0].attributes.command
   if (commandText === "") {
     throw new Error('empty "command" attribute')
   }
